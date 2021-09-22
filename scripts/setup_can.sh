ed3f2ed2a4f437a483bcdc2840699d7fa4778655
Derek Maggio <DerekMaggio@users.noreply.github.com>
2021-09-22 07:48:27 -0700

    chore: Make OT-3 Emulator setup and teardown more configurable (#13)
    * Remove python-can service
    
    * Add cleanup to build_system to make it a repeatable command
    
    * Add naming network and CANFD usage
    
    * Make pull-from optional to allow pulling from main

--- scripts/setup_can.sh
+++ scripts/setup_can.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # This script will create a SocketCan network, vcan0
 
-sudo ip link add dev vcan0 type vcan
+if [ -z "${CAN_CHANNEL}" ]; then
+  NETWORK="vcan0"
+else
+  NETWORK=${CAN_CHANNEL}
+fi
+
+
+
+sudo ip link add dev $NETWORK type vcan fd on
 if [ $? == 2 ]; then
-  echo "CAN Virtual Network already exists"
+  echo "CAN Virtual Network \"$NETWORK\" already exists"
   exit 1
 fi
 
-sudo ip link set up vcan0
+sudo ip link set up $NETWORK
 
 if [ $? == 0 ]; then
-  echo "CAN Virtual Network successfully created"
+  echo "CAN Virtual Network \"$NETWORK\" successfully created"
 else
-  echo "Error creating CAN Virtual Network"
+  echo "Error creating CAN Virtual Network \"$NETWORK\""
 fi
 
