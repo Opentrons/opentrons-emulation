17ddc18023c1f3b54699850ed9257c7e1993466b
Derek Maggio <derek.maggio@opentrons.com>
2021-09-22 07:40:05 -0700

    Add naming network and CANFD usage

--- scripts/teardown_can.sh
+++ scripts/teardown_can.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # This script will delete SocketCan network vcan0
 
-sudo ip link delete vcan0 2> /dev/null
+if [ -z "${CAN_CHANNEL}" ]; then
+  NETWORK="vcan0"
+else
+  NETWORK=${CAN_CHANNEL}
+fi
+
+
+sudo ip link delete $NETWORK 2> /dev/null
 if [ $? == 1 ]; then
-  echo "CAN Virtual Network does not exist"
+  echo "CAN Virtual Network \"$NETWORK\" does not exist"
   exit 1
 fi
 
-echo "CAN Virtual Network successfully removed"
+echo "CAN Virtual Network \"$NETWORK\" successfully removed"
