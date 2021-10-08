8772d9018b83555d617dd1cf3a6e723ecc5d726a
Derek Maggio <derek.maggio@opentrons.com>
2021-10-08 13:22:43 -0700

    Remove extraneous comments

--- scripts/run_emulation.sh
+++ scripts/run_emulation.sh
@@ -1,148 +1,10 @@
 #!/usr/bin/env bash
-#       _                 _
-#   ___(_)_ __ ___  _ __ | | ___   _
-#  / __| | '_ ` _ \| '_ \| |/ _ \_| |_
-#  \__ \ | | | | | | |_) | |  __/_   _|
-#  |___/_|_| |_| |_| .__/|_|\___| |_|
-#                  |_|
-#
-# Boilerplate for creating a simple bash script with some basic strictness
-# checks, help features, easy debug printing.
-#
-# Usage:
-#   bash-simple-plus <options>...
-#
-# Depends on:
-#  list
-#  of
-#  programs
-#  expected
-#  in
-#  environment
-#
-# Bash Boilerplate: https://github.com/xwmx/bash-boilerplate
-#
-# Copyright (c) 2015 William Melody • hi@williammelody.com
 
-# Notes #######################################################################
-
-# Extensive descriptions are included for easy reference.
-#
-# Explicitness and clarity are generally preferable, especially since bash can
-# be difficult to read. This leads to noisier, longer code, but should be
-# easier to maintain. As a result, some general design preferences:
-#
-# - Use leading underscores on internal variable and function names in order
-#   to avoid name collisions. For unintentionally global variables defined
-#   without `local`, such as those defined outside of a function or
-#   automatically through a `for` loop, prefix with double underscores.
-# - Always use braces when referencing variables, preferring `${NAME}` instead
-#   of `$NAME`. Braces are only required for variable references in some cases,
-#   but the cognitive overhead involved in keeping track of which cases require
-#   braces can be reduced by simply always using them.
-# - Prefer `printf` over `echo`. For more information, see:
-#   http://unix.stackexchange.com/a/65819
-# - Prefer `$_explicit_variable_name` over names like `$var`.
-# - Use the `#!/usr/bin/env bash` shebang in order to run the preferred
-#   Bash version rather than hard-coding a `bash` executable path.
-# - Prefer splitting statements across multiple lines rather than writing
-#   one-liners.
-# - Group related code into sections with large, easily scannable headers.
-# - Describe behavior in comments as much as possible, assuming the reader is
-#   a programmer familiar with the shell, but not necessarily experienced
-#   writing shell scripts.
-
-###############################################################################
-# Strict Mode
-###############################################################################
-
-# Treat unset variables and parameters other than the special parameters ‘@’ or
-# ‘*’ as an error when performing parameter expansion. An 'unbound variable'
-# error message will be written to the standard error, and a non-interactive
-# shell will exit.
-#
-# This requires using parameter expansion to test for unset variables.
-#
-# http://www.gnu.org/software/bash/manual/bashref.html#Shell-Parameter-Expansion
-#
-# The two approaches that are probably the most appropriate are:
-#
-# ${parameter:-word}
-#   If parameter is unset or null, the expansion of word is substituted.
-#   Otherwise, the value of parameter is substituted. In other words, "word"
-#   acts as a default value when the value of "$parameter" is blank. If "word"
-#   is not present, then the default is blank (essentially an empty string).
-#
-# ${parameter:?word}
-#   If parameter is null or unset, the expansion of word (or a message to that
-#   effect if word is not present) is written to the standard error and the
-#   shell, if it is not interactive, exits. Otherwise, the value of parameter
-#   is substituted.
-#
-# Examples
-# ========
-#
-# Arrays:
-#
-#   ${some_array[@]:-}              # blank default value
-#   ${some_array[*]:-}              # blank default value
-#   ${some_array[0]:-}              # blank default value
-#   ${some_array[0]:-default_value} # default value: the string 'default_value'
-#
-# Positional variables:
-#
-#   ${1:-alternative} # default value: the string 'alternative'
-#   ${2:-}            # blank default value
-#
-# With an error message:
-#
-#   ${1:?'error message'}  # exit with 'error message' if variable is unbound
-#
-# Short form: set -u
 set -o nounset
-
-# Exit immediately if a pipeline returns non-zero.
-#
-# NOTE: This can cause unexpected behavior. When using `read -rd ''` with a
-# heredoc, the exit status is non-zero, even though there isn't an error, and
-# this setting then causes the script to exit. `read -rd ''` is synonymous with
-# `read -d $'\0'`, which means `read` until it finds a `NUL` byte, but it
-# reaches the end of the heredoc without finding one and exits with status `1`.
-#
-# Two ways to `read` with heredocs and `set -e`:
-#
-# 1. set +e / set -e again:
-#
-#     set +e
-#     read -rd '' variable <<HEREDOC
-#     HEREDOC
-#     set -e
-#
-# 2. Use `<<HEREDOC || true:`
-#
-#     read -rd '' variable <<HEREDOC || true
-#     HEREDOC
-#
-# More information:
-#
-# https://www.mail-archive.com/bug-bash@gnu.org/msg12170.html
-#
-# Short form: set -e
 set -o errexit
-
-# Allow the above trap be inherited by all functions in the script.
-#
-# Short form: set -E
 set -o errtrace
-
-# Return value of a pipeline is the value of the last (rightmost) command to
-# exit with a non-zero status, or zero if all commands in the pipeline exit
-# successfully.
 set -o pipefail
 
-# Set $IFS to only newline and tab.
-#
-# http://www.dwheeler.com/essays/filenames-in-shell.html
 IFS=$'\n\t'
 
 ###############################################################################
@@ -261,25 +123,8 @@ HEREDOC
 }
 
 ###############################################################################
-# Options
-#
-# NOTE: The `getops` builtin command only parses short options and BSD `getopt`
-# does not support long arguments (GNU `getopt` does), so the most portable
-# and clear way to parse options is often to just use a `while` loop.
-#
-# For a pure bash `getopt` function, try pure-getopt:
-#   https://github.com/agriffis/pure-getopt
-#
-# More info:
-#   http://wiki.bash-hackers.org/scripting/posparams
-#   http://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html
-#   http://stackoverflow.com/a/14203146
-#   http://stackoverflow.com/a/7948533
-#   https://stackoverflow.com/a/12026302
-#   https://stackoverflow.com/a/402410
+# Parse Options
 ###############################################################################
-
-# Parse Options ###############################################################
 
 # Initialize program option variables.
 _PRINT_HELP=0
