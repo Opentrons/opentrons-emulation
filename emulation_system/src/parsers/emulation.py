04d3a9d9b5931794af42e05b0d138023f12a9bc9
Derek Maggio <derek.maggio@opentrons.com>
2021-10-29 12:25:42 -0700

    Break out dev and prod emulation options

--- emulation_system/src/parsers/emulation.py
+++ emulation_system/src/parsers/emulation.py
@@ -1,10 +1,17 @@
 import argparse
 from parser_utils import ParserWithError, get_formatter
 from script_creators.emulation_creator import (
-    EmulationOptions,
+    CommonEmulationOptions,
+    ProductionEmulationOptions,
+    DevelopmentEmulationOptions,
     EmulationSubCommands,
     ProdEmulationCreator,
     DevEmulationCreator
+)
+from consts import (
+    OPENTRONS_MODULES_REPO_DEFAULT_PATH,
+    OT3_FIRMWARE_REPO_DEFAULT_PATH,
+    OPENTRONS_REPO_DEFAULT_PATH
 )
 
 
@@ -24,7 +31,7 @@ def emulation_parser(parser: ParserWithError) -> None:
     common_parser = argparse.ArgumentParser()
 
     common_parser.add_argument(
-        f"--{EmulationOptions.DETACHED.value}",
+        f"--{CommonEmulationOptions.DETACHED.value}",
         action="store_true",
         help="Release stdout after emulation system creation"
     )
@@ -33,31 +40,52 @@ def emulation_parser(parser: ParserWithError) -> None:
         EmulationSubCommands.PROD_MODE.value,
         help="Create production system",
         parents=[common_parser],
-        conflict_handler='resolve'
+        conflict_handler='resolve',
+        formatter_class=get_formatter()
     )
     prod_parser.set_defaults(func=ProdEmulationCreator.from_cli_input)
     prod_parser.add_argument(
-        f"--{EmulationOptions.OT3_FIRMWARE_SHA.value}",
+        f"--{ProductionEmulationOptions.OT3_FIRMWARE_SHA.value}",
         action="store",
-        help=(
-            "Commit ID to download from ot3-firmware "
-            "(Only for \"prod\" systems)"
-        ),
-
+        help="Commit ID to download from ot3-firmware repo",
+        metavar="<commit-sha>"
     )
     prod_parser.add_argument(
-        f'--{EmulationOptions.MODULES_SHA.value}',
+        f'--{ProductionEmulationOptions.MODULES_SHA.value}',
         action="store",
-        help=(
-            "Commit ID to download from opentrons-modules "
-            "(Only for \"prod\" systems)"
-        )
+        help="Commit ID to download from opentrons-modules repo",
+        metavar="<commit-sha>"
+    )
+    prod_parser.add_argument(
+        f'--{ProductionEmulationOptions.MONOREPO_SHA.value}',
+        action="store",
+        help="Commit ID to download from opentrons repo",
+        metavar="<commit-sha>"
     )
 
     dev_parser = sub_subparser.add_parser(
         EmulationSubCommands.DEV_MODE.value,
         help="Create development system",
         parents=[common_parser],
-        conflict_handler='resolve'
+        conflict_handler='resolve',
+        formatter_class=get_formatter()
     )
     dev_parser.set_defaults(func=DevEmulationCreator.from_cli_input)
+    dev_parser.add_argument(
+        DevelopmentEmulationOptions.MODULES_PATH.value,
+        help="Path to opentrons-modules repo source code",
+        default=OPENTRONS_MODULES_REPO_DEFAULT_PATH,
+        metavar="<absolute_path>"
+    )
+    dev_parser.add_argument(
+        DevelopmentEmulationOptions.OT3_FIRMWARE_PATH.value,
+        help="Path to ot3-firmware repo source code",
+        default=OT3_FIRMWARE_REPO_DEFAULT_PATH,
+        metavar="<absolute_path>"
+    )
+    dev_parser.add_argument(
+        DevelopmentEmulationOptions.OPENTRONS_REPO.value,
+        help="Path to opentrons repo source code",
+        default=OPENTRONS_REPO_DEFAULT_PATH,
+        metavar="<absolute_path>"
+    )
--- emulation_system/src/script_creators/emulation_creator.py
+++ emulation_system/src/script_creators/emulation_creator.py
@@ -6,16 +6,25 @@ from consts import (
     DEVELOPMENT_MODE_NAME
 )
 
-
 class EmulationSubCommands(str, Enum):
     PROD_MODE = PRODUCTION_MODE_NAME
     DEV_MODE = DEVELOPMENT_MODE_NAME
 
 
-class EmulationOptions(str, Enum):
+class CommonEmulationOptions(str, Enum):
     DETACHED = "detached"
-    OT3_FIRMWARE_SHA = 'ot3-firmware-sha'
-    MODULES_SHA = 'modules-sha'
+
+
+class ProductionEmulationOptions(str, Enum):
+    OT3_FIRMWARE_SHA = 'ot3-firmware-repo-sha'
+    MODULES_SHA = 'opentrons-modules-repo-sha'
+    MONOREPO_SHA = 'opentrons-repo-sha'
+
+
+class DevelopmentEmulationOptions(str, Enum):
+    MODULES_PATH = "--opentrons-modules-repo-path"
+    OT3_FIRMWARE_PATH = "--ot3-firmware-repo-path"
+    OPENTRONS_REPO = "--opentrons-repo-path"
 
 
 class InvalidModeError(ValueError):
@@ -27,22 +36,30 @@ class ProdEmulationCreator:
     detached: bool = False
     ot3_firmware_sha: str = ''
     modules_sha: str = ''
+    opentrons_sha: str = ''
 
     @classmethod
     def from_cli_input(cls, args) -> ProdEmulationCreator:
         return cls(
             detached=args.detached,
-            ot3_firmware_sha=args.ot3_firmware_sha,
-            modules_sha=args.modules_sha
+            ot3_firmware_sha=args.ot3_firmware_repo_sha,
+            modules_sha=args.opentrons_modules_repo_sha,
+            opentrons_sha=args.opentrons_repo_sha
         )
 
 
 @dataclass
 class DevEmulationCreator:
     detached: bool = False
+    ot3_firmware_path: str = ''
+    modules_path: str = ''
+    opentrons_path: str = ''
 
     @classmethod
     def from_cli_input(cls, args) -> DevEmulationCreator:
         return cls(
-            detached=args.detached
+            detached=args.detached,
+            ot3_firmware_path=args.ot3_firmware_repo_path,
+            modules_path=args.opentrons_modules_repo_path,
+            opentrons_path=args.opentrons_repo_path
         )
