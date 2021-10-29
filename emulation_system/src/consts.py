93be5e62c6c9bcc2fbfaea984e98173e3cdf939a
Derek Maggio <derek.maggio@opentrons.com>
2021-10-29 13:36:17 -0700

    Add repo root const

--- emulation_system/src/consts.py
+++ emulation_system/src/consts.py
@@ -1,3 +1,5 @@
+import os
+
 # Source Code Paths
 OPENTRONS_MODULES_REPO_DEFAULT_PATH = "/home/opentrons-modules"
 OT3_FIRMWARE_REPO_DEFAULT_PATH = "/home/ot3-firmware"
@@ -6,3 +8,7 @@ OPENTRONS_REPO_DEFAULT_PATH = "/home/opentrons"
 # Mode Names
 PRODUCTION_MODE_NAME = 'prod'
 DEVELOPMENT_MODE_NAME = 'dev'
+
+# Root of repo
+
+ROOT_DIR = os.path.relpath("../../../opentrons-emulation")
