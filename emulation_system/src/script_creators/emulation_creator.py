09468c87b8e1fdb6eb329913891dd63e2c0b25ea
Derek Maggio <derek.maggio@opentrons.com>
2021-10-29 11:41:12 -0700

    Breakout into prod and dev Emulation Creators

--- emulation_system/src/script_creators/emulation_creator.py
+++ emulation_system/src/script_creators/emulation_creator.py
@@ -3,8 +3,13 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Dict
 
+
+class EmulationSubCommands(str, Enum):
+    PROD_MODE = "prod"
+    DEV_MODE = "dev"
+
+
 class EmulationOptions(str, Enum):
-    MODE = "mode"
     DETACHED = "detached"
     OT3_FIRMWARE_SHA = 'ot3-firmware-sha'
     MODULES_SHA = 'modules-sha'
@@ -13,27 +18,28 @@ class EmulationOptions(str, Enum):
 class InvalidModeError(ValueError):
     pass
 
-@dataclass
-class EmulationCreator:
-    PROD_MODE = "prod"
-    DEV_MODE = "dev"
 
-    mode: str
+@dataclass
+class ProdEmulationCreator:
     detached: bool = False
     ot3_firmware_sha: str = ''
     modules_sha: str = ''
 
     @classmethod
-    def from_cli_input(cls, args) -> EmulationCreator:
+    def from_cli_input(cls, args) -> ProdEmulationCreator:
         return cls(
-            mode=args.mode,
             detached=args.detached,
             ot3_firmware_sha=args.ot3_firmware_sha,
             modules_sha=args.modules_sha
         )
 
-    def __post_init__(self):
-        if self.mode not in [self.PROD_MODE, self.DEV_MODE]:
-            raise InvalidModeError(
-                f"\"mode\" must either be {self.PROD_MODE} or {self.DEV_MODE}"
-            )
+
+@dataclass
+class DevEmulationCreator:
+    detached: bool = False
+
+    @classmethod
+    def from_cli_input(cls, args) -> DevEmulationCreator:
+        return cls(
+            detached=args.detached
+        )
