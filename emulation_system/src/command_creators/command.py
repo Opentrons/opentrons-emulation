497a5d057222d79fcb23da0cacb706377039d609
Derek Maggio <derek.maggio@opentrons.com>
2021-11-01 10:50:48 -0700

    Command classes

---
+++ emulation_system/src/command_creators/command.py
@@ -0,0 +1,59 @@
+import subprocess
+from dataclasses import dataclass
+from typing import List
+from consts import ROOT_DIR
+
+
+class CommandExecutionError(ValueError):
+    """Thrown when there is an error executing a command"""
+    pass
+
+
+@dataclass
+class Command:
+    """Command to be run by subprocess"""
+    command_name: str
+    command: str
+
+    def run_command(self):
+        try:
+            command_output = subprocess.run(
+                self.command,
+                check=True,
+                shell=True,
+                cwd=ROOT_DIR,
+                capture_output=True
+            )
+        except subprocess.CalledProcessError as err:
+            raise CommandExecutionError(err.stderr)
+        else:
+            return CommandOutput(
+                command_name=self.command_name,
+                command=command_output.args,
+                output=command_output.stdout.decode("utf-8").strip()
+            )
+
+
+@dataclass
+class CommandOutput:
+    """Output from command run by subprocess"""
+    command_name: str
+    command: str
+    output: str
+
+
+@dataclass
+class CommandList:
+    """List of commands for subprocess to run"""
+    command_list: List[Command]
+
+    def add_command(self, command: Command):
+        self.command_list.append(command)
+
+    def run_commands(self):
+        return [
+            command.run_command()
+            for command
+            in self.command_list
+        ]
+
