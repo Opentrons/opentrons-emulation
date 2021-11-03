349345863339e59e0010f066639da55e2929e141
Derek Maggio <derek.maggio@opentrons.com>
2021-11-03 09:41:29 -0700

    Simplify main

--- emulation_system/src/main.py
+++ emulation_system/src/main.py
@@ -1,7 +1,4 @@
-import sys
-from parsers.top_level import top_level_parser
+from parsers.top_level_parser import TopLevelParser
 
 if __name__ == "__main__":
-    parser = top_level_parser()
-    args = parser.parse_args(sys.argv[1:])
-    print(args.func(args))
+    TopLevelParser().parse()
