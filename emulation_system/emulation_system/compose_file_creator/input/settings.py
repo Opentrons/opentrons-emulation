0906811c7b81f6c669053095b6a679a024e43f31
Derek Maggio <derek.maggio@opentrons.com>
2021-12-13 11:01:17 -0800

    Update settings and add docstrings

--- emulation_system/emulation_system/compose_file_creator/input/settings.py
+++ emulation_system/emulation_system/compose_file_creator/input/settings.py
@@ -1,34 +1,39 @@
 from enum import Enum
 from pydantic import BaseModel, Field
 
-ROOM_TEMPERATURE = 23
+ROOM_TEMPERATURE = 23.0
 
 
 class Hardware(str, Enum):
-    HEATER_SHAKER_MODULE = "Heater Shaker Module"
-    THERMOCYCLER_MODULE = "Thermocycler Module"
-    TEMPERATURE_MODULE = "Temperature Module"
-    MAGNETEIC_MODULE = "Magnetic Module"
-    OT2 = "OT-2"
-    OT3 = "OT-3"
+    """Hardware supported by emulation"""
+    HEATER_SHAKER = "heater-shaker-module"
+    THERMOCYCLER = "thermocycler-module"
+    TEMPERATURE = "temperature-module"
+    MAGNETEIC = "magnetic-module"
+    OT2 = "ot2"
+    OT3 = "ot3"
 
 
 class EmulationLevel(str, Enum):
+    """The emulation level of the emulator"""
     FIRMWARE = "firmware"
     DRIVER = "driver"
 
 
 class SourceType(str, Enum):
+    """Where to pull source from"""
     REMOTE = "remote"
     LOCAL = "local"
 
 
 class HeaterShakerModes(str, Enum):
+    """Where to read G-Codes from"""
     STDIN = "stdin"
     SOCKET = "socket"
 
 
 class TemperatureModelSettings(BaseModel):
+    """Temperature behavior model"""
     degrees_per_tick: float = Field(
         alias="degrees-per-tick", default=2.0
     )
@@ -36,5 +41,6 @@ class TemperatureModelSettings(BaseModel):
 
 
 class PipetteSettings(BaseModel):
+    """Pipette defintions for OT-2 and OT-3"""
     model: str = "p20_single_v2.0"
     id: str = "P20SV202020070101"
