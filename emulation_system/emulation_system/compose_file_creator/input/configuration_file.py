3b8cae1e9b445240e4877d7d7beab4e53421f3b4
Derek Maggio <derek.maggio@opentrons.com>
2021-12-13 14:00:28 -0800

    Refactor to use validators

--- emulation_system/emulation_system/compose_file_creator/input/configuration_file.py
+++ emulation_system/emulation_system/compose_file_creator/input/configuration_file.py
@@ -2,8 +2,11 @@ from __future__ import annotations
 
 import json
 import os
-from typing import List, Union, Optional
-from pydantic import BaseModel, ValidationError, parse_obj_as
+from typing import List, Union, Optional, Dict
+from pydantic import BaseModel, ValidationError, parse_obj_as, validator, \
+    root_validator, parse_file_as
+
+from compose_file_creator.input.hardware_models import MagneticModuleModel
 from emulation_system.consts import ROOT_DIR
 from emulation_system.compose_file_creator.input.hardware_models\
     .modules.module_model import ModuleModel
@@ -39,6 +42,7 @@ class ModuleParser(BaseModel):
         HeaterShakerModuleModel,
         ThermocyclerModuleModel,
         TemperatureModuleModel,
+        MagneticModuleModel
     ]
 
     @staticmethod
@@ -53,53 +57,39 @@ class SystemConfigurationModel(BaseModel):
     Represents an entire system to be brought up"""
     _ROBOT_IDENTIFIER = "robot"
     _MODULES_IDENTIFIER = "modules"
-    robot: Optional[RobotModel]
-    modules: Optional[List[ModuleModel]]
-    _container_name_list = []
+    robot: Optional[Dict[str, RobotParser]]
+    modules: Optional[Dict[str, ModuleParser]]
 
-    @classmethod
-    def parse_robot(cls, robot) -> RobotModel:
+    class Config:
+        extra = "forbid"
+
+    @validator("robot", pre=True)
+    def add_robot_ids(cls, value) -> None:
         """Parses robot object in JSON file to a RobotModel object"""
-        robot_id = list(robot.keys())[0]
-        robot_content = robot[robot_id]
-        robot_content['id'] = robot_id
-        return RobotParser.parse_to_robot(robot_content)
+        robot_id = list(value.keys())[0]
+        value[robot_id]['id'] = robot_id
+        return value
 
-    @classmethod
-    def parse_modules(cls, modules) -> List[ModuleModel]:
+    @validator("modules", pre=True)
+    def add_module_ids(cls, value) -> None:
         """Parses all modules in JSON file to a list of ModuleModels"""
-        module_list = []
-        for key, module in modules.items():
+        for key, module in value.items():
             module['id'] = key
-            module_list.append(
-                ModuleParser.parse_to_module(module)
-            )
-        return module_list
+        return value
 
-    @classmethod
-    def from_file(cls, file_location: str) -> SystemConfigurationModel:
-        """Parses JSON file into a SystemConfigurationModel"""
-        content = json.load(open(file_location, 'r'))
-
-        robot = None
-        module_list = []
-
-        if cls._MODULES_IDENTIFIER in content:
-            module_list = cls.parse_modules(content[cls._MODULES_IDENTIFIER])
-
-        if cls._ROBOT_IDENTIFIER in content:
-            robot = cls.parse_robot(content[cls._ROBOT_IDENTIFIER])
-
-        return SystemConfigurationModel(
-            robot=robot,
-            modules=module_list
-        )
+    @root_validator
+    def remove_root_key(cls, values):
+        for k, v in values['robot'].items():
+            values['robot'][k] = v.__root__
+        for k, v in values['modules'].items():
+            values['modules'][k] = v.__root__
+        return values
 
 
 if __name__ == "__main__":
     try:
         location = os.path.join(ROOT_DIR, "emulation_system/resources/new_config.json")
-        system_configuration = SystemConfigurationModel.from_file(location)
+        system_configuration = parse_file_as(SystemConfigurationModel, location)
         print(system_configuration)
     except ValidationError as e:
         print(e)
