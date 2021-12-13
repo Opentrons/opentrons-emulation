a6dfff18b321e3172ad36712df917efebad379be
Derek Maggio <derek.maggio@opentrons.com>
2021-12-13 08:10:08 -0800

    Cleanup imports

--- emulation_system/emulation_system/compose_file_creator/input/hardware_models/__init__.py
+++ emulation_system/emulation_system/compose_file_creator/input/hardware_models/__init__.py
@@ -1,11 +1,5 @@
 from emulation_system.compose_file_creator.input.hardware_models\
-    .hardware_model import HardwareModel
-from emulation_system.compose_file_creator.input.hardware_models\
-    .robots.robot_model import RobotModel
-from emulation_system.compose_file_creator.input.hardware_models\
     .robots.ot2_model import OT2Model
-from emulation_system.compose_file_creator.input.hardware_models\
-    .modules.module_model import ModuleModel
 from emulation_system.compose_file_creator.input.hardware_models\
     .modules.heater_shaker_module import HeaterShakerModuleModel
 from emulation_system.compose_file_creator.input.hardware_models\
