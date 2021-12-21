edff6b6b2a59176b69a77a1ab80ab3ea8c09a3e8
Derek Maggio <derek.maggio@opentrons.com>
2021-12-21 11:59:26 -0800

    Add tests for modules_exist and robot_exists properties

--- emulation_system/tests/compose_file_creator/input/test_configuration_file.py
+++ emulation_system/tests/compose_file_creator/input/test_configuration_file.py
@@ -17,20 +17,20 @@ from emulation_system.compose_file_creator.input.configuration_file import (
 def matching_robot_and_module_names() -> Dict:
     """Configuration file with matching robot and module name."""
     return {
-        "robot": {
+        "robot":   {
             "common-name": {
-                "hardware": "ot2",
+                "hardware":        "ot2",
                 "emulation-level": "firmware",
-                "source-type": "remote",
+                "source-type":     "remote",
                 "source-location": "latest",
             }
         },
         "modules": {
             "common-name": {
-                "hardware": "heater-shaker-module",
-                "emulation-level": "hardware",
-                "source-type": "remote",
-                "source-location": "latest",
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
                 "hardware-specific-attributes": {"mode": "stdin"},
             }
         },
@@ -43,10 +43,10 @@ def invalid_name_format() -> Dict:
     return {
         "modules": {
             "invalid name with spaces": {
-                "hardware": "heater-shaker-module",
-                "emulation-level": "hardware",
-                "source-type": "remote",
-                "source-location": "latest",
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
                 "hardware-specific-attributes": {"mode": "stdin"},
             }
         }
@@ -59,17 +59,86 @@ def multiple_robots() -> Dict:
     return {
         "robot": {
             "robot-1": {
-                "hardware": "ot2",
+                "hardware":        "ot2",
                 "emulation-level": "firmware",
-                "source-type": "remote",
+                "source-type":     "remote",
                 "source-location": "latest",
             },
             "robot-2": {
-                "hardware": "ot2",
+                "hardware":        "ot2",
                 "emulation-level": "firmware",
-                "source-type": "remote",
+                "source-type":     "remote",
                 "source-location": "latest",
             },
+        }
+    }
+
+
+@pytest.fixture
+def modules_only() -> Dict:
+    """Configuration with only modules."""
+    return {
+        "modules": {
+            "my-heater-shaker":   {
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
+                "hardware-specific-attributes": {"mode": "stdin"},
+            },
+            "my-heater-shaker-2": {
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
+                "hardware-specific-attributes": {"mode": "stdin"},
+            }
+        }
+    }
+
+
+@pytest.fixture
+def robot_only() -> Dict:
+    """Configuration with only modules."""
+    return {
+        "robot": {
+            "my-robot": {
+                "hardware":        "ot2",
+                "emulation-level": "firmware",
+                "source-type":     "remote",
+                "source-location": "latest",
+            }
+        }
+    }
+
+
+@pytest.fixture
+def robot_and_modules() -> Dict:
+    """Configuration with robot and modules."""
+    return {
+        "robot":   {
+            "my-robot": {
+                "hardware":        "ot2",
+                "emulation-level": "firmware",
+                "source-type":     "remote",
+                "source-location": "latest",
+            }
+        },
+        "modules": {
+            "my-heater-shaker":   {
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
+                "hardware-specific-attributes": {"mode": "stdin"},
+            },
+            "my-heater-shaker-2": {
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
+                "hardware-specific-attributes": {"mode": "stdin"},
+            }
         }
     }
 
@@ -104,3 +173,35 @@ def test_multiple_robots(multiple_robots: Dict) -> None:
         create_system_configuration(multiple_robots)
     expected_error_text = "You can only define 1 robot"
     assert err.match(expected_error_text)
+
+
+@pytest.mark.parametrize(
+    "config_dict", [
+        pytest.lazy_fixture("modules_only"),
+        pytest.lazy_fixture("robot_and_modules"),
+    ]
+    )
+def test_modules_exist_is_true(config_dict: Dict) -> None:
+    """Test that modules_exist property is true when it is supposed to be."""
+    assert create_system_configuration(config_dict).modules_exist
+
+
+def test_modules_exist_is_false(robot_only: Dict) -> None:
+    """Test that modules_exist property is false when it is supposed to be."""
+    assert not create_system_configuration(robot_only).modules_exist
+
+
+@pytest.mark.parametrize(
+    "config_dict", [
+        pytest.lazy_fixture("robot_only"),
+        pytest.lazy_fixture("robot_and_modules"),
+    ]
+    )
+def test_robot_exists_is_true(config_dict: Dict) -> None:
+    """Test that robot_exists property is true when it is supposed to be."""
+    assert create_system_configuration(config_dict).robot_exists
+
+
+def test_robot_exists_is_false(modules_only: Dict) -> None:
+    """Test that robot_exists property is false when it is supposed to be."""
+    assert not create_system_configuration(modules_only ).robot_exists
