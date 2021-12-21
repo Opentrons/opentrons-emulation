5a3deb126e86d344b0a8a6364e0df5b89e243871
Derek Maggio <derek.maggio@opentrons.com>
2021-12-21 11:59:26 -0800

    Add tests for modules_exist and robot_exists properties

--- emulation_system/tests/compose_file_creator/input/test_configuration_file.py
+++ emulation_system/tests/compose_file_creator/input/test_configuration_file.py
@@ -24,20 +24,20 @@ from emulation_system.compose_file_creator.input.hardware_models import (
 def matching_robot_and_module_names() -> Dict:
     """Configuration file with matching robot and module name."""
     return {
-        "robot": {
+        "robot":   {
             "common-name": {
-                "hardware": "ot2",
+                "hardware":        "ot2",
                 "emulation-level": "firmware",
-                "source-type": "remote",
+                "source-type":     "remote",
                 "source-location": "latest",
             }
         },
         "modules": {
             "common-name": {
-                "hardware": "heater-shaker-module",
-                "emulation-level": "hardware",
-                "source-type": "remote",
-                "source-location": "latest",
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
                 "hardware-specific-attributes": {"mode": "stdin"},
             }
         },
@@ -50,10 +50,10 @@ def invalid_name_format() -> Dict:
     return {
         "modules": {
             "invalid name with spaces": {
-                "hardware": "heater-shaker-module",
-                "emulation-level": "hardware",
-                "source-type": "remote",
-                "source-location": "latest",
+                "hardware":                     "heater-shaker-module",
+                "emulation-level":              "hardware",
+                "source-type":                  "remote",
+                "source-location":              "latest",
                 "hardware-specific-attributes": {"mode": "stdin"},
             }
         }
@@ -66,15 +66,15 @@ def multiple_robots() -> Dict:
     return {
         "robot": {
             "robot-1": {
-                "hardware": "ot2",
+                "hardware":        "ot2",
                 "emulation-level": "firmware",
-                "source-type": "remote",
+                "source-type":     "remote",
                 "source-location": "latest",
             },
             "robot-2": {
-                "hardware": "ot2",
+                "hardware":        "ot2",
                 "emulation-level": "firmware",
-                "source-type": "remote",
+                "source-type":     "remote",
                 "source-location": "latest",
             },
         }
