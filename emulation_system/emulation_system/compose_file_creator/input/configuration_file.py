08249d53d7a8c13dbb27d40266b71fec95a905c4
Derek Maggio <derek.maggio@opentrons.com>
2021-12-21 11:02:10 -0800

    configuration_file.py Updates
    Only check for duplicate names if robot and modules are defined
    Only allow 1 robot
    Add modules_exist and robot_exists properties
    Add get_by_id, from file, and from_dict methods

--- emulation_system/emulation_system/compose_file_creator/input/configuration_file.py
+++ emulation_system/emulation_system/compose_file_creator/input/configuration_file.py
@@ -12,6 +12,7 @@ from pydantic import (
     Field,
     ValidationError,
     parse_file_as,
+    parse_obj_as,
     root_validator,
     validator,
 )
@@ -41,17 +42,32 @@ class SystemConfigurationModel(BaseModel):
     @root_validator
     def validate_names(cls, values) -> Dict[str, Dict[str, Containers]]:  # noqa: ANN001
         """Checks all names in the config file and confirms there are no duplicates."""
-        robot_names = set(values["robot"].keys())
-        module_names = set(values["modules"].keys())
+        # Only need to check when there is both a robot and modules defined.
+        # This is because if there are duplicates in modules then it will fail by
+        # definition of how a dict works (no duplicate keys)
+        if "robot" in values and "modules" in values:
+            robot_names = set(values["robot"].keys())
+            module_names = set(values["modules"].keys())
 
-        name_intersections = robot_names.intersection(module_names)
-        assert len(name_intersections) == 0, (
-            "The following container names are "
-            "duplicated in the configuration file: "
-            f"{', '.join(name_intersections)}"
-        )
+            name_intersections = robot_names.intersection(module_names)
+            assert len(name_intersections) == 0, (
+                "The following container names are "
+                "duplicated in the configuration file: "
+                f"{', '.join(name_intersections)}"
+            )
 
         return values
+
+    @validator("robot", pre=True)
+    def there_can_only_be_one(cls, value) -> Dict[str, Robots]:  # noqa: ANN001
+        """Confirm that there is only 1 robot defined.
+
+        Was hoping to do this with the maxproperties property in the JSONSchema,
+        but it seems that pydantic does not support it so I have to do it with
+        a validator.
+        """
+        assert len(value) == 1, "You can only define 1 robot"
+        return value
 
     @validator("robot", pre=True)
     def add_robot_ids(cls, value) -> Dict[str, Robots]:  # noqa: ANN001
@@ -59,6 +75,8 @@ class SystemConfigurationModel(BaseModel):
         robot_id = list(value.keys())[0]
         value[robot_id]["id"] = robot_id
         return value
+
+
 
     @validator("modules", pre=True)
     def add_module_ids(cls, value) -> Dict[str, Modules]:  # noqa: ANN001
@@ -68,6 +86,16 @@ class SystemConfigurationModel(BaseModel):
         return value
 
     @property
+    def modules_exist(self) -> bool:
+        """Returns True if modules were defined in config file, False if not."""
+        return len(self.modules) > 0
+
+    @property
+    def robot_exists(self) -> bool:
+        """Returns True if a robot was defined in config file, False if not."""
+        return len(self.robot) > 0
+
+    @property
     def containers(self) -> Mapping[str, Containers]:
         """Return all robots and modules in a single dictionary."""
         new_dict: Dict[str, Containers] = {}
@@ -75,16 +103,41 @@ class SystemConfigurationModel(BaseModel):
         new_dict.update(self.modules)
         return new_dict
 
+    def get_by_id(self, container_id: str) -> Containers:
+        """Return hardware model by container id."""
+        return self.containers[container_id]
+
+    @classmethod
+    def from_file(cls, file_path: str) -> SystemConfigurationModel:
+        """Parse from file."""
+        return parse_file_as(cls, file_path)
+
+    @classmethod
+    def from_dict(cls, obj: Dict) -> SystemConfigurationModel:
+        """Parse from dict."""
+        return parse_obj_as(cls, obj)
+
 
 if __name__ == "__main__":
     try:
         location = os.path.join(ROOT_DIR, "emulation_system/resources/new_config.json")
-        system_configuration = parse_file_as(SystemConfigurationModel, location)
-        for module in system_configuration.containers.values():
-            print(
-                f"{module.hardware}, "
-                f"{module.source_type}, "
-                f"{module.emulation_level}: "
-            )
+        system_configuration = SystemConfigurationModel.from_file(location)
+
+        if system_configuration.modules_exist:
+            for module in system_configuration.modules.values():
+                print(
+                    f"{module.id}, "
+                    f"{module.hardware}, "
+                    f"{module.source_type}, "
+                    f"{module.emulation_level}: "
+                )
+        if system_configuration.robot_exists:
+            for robot in system_configuration.robot.values():
+                print(
+                    f"{robot.id}, "
+                    f"{robot.hardware}, "
+                    f"{robot.source_type}, "
+                    f"{robot.emulation_level}: "
+                )
     except ValidationError as e:
         print(e)
