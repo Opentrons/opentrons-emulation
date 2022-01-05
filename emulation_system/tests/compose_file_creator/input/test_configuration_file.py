757b09c33d2e6c52b81a5e89c7883d39a0ee0f97
amit lissack <amit@opentrons.com>
2022-01-05 09:58:31 -0500

    fix lint

--- emulation_system/tests/compose_file_creator/input/test_configuration_file.py
+++ emulation_system/tests/compose_file_creator/input/test_configuration_file.py
@@ -10,8 +10,6 @@ from typing import Dict
 import pytest
 from pydantic import ValidationError
 from pytest_lazyfixture import lazy_fixture  # type: ignore
-from pydantic import ValidationError
-from pytest_lazyfixture import lazy_fixture  # type: ignore
 
 from emulation_system.compose_file_creator.input.configuration_file import (
     SystemConfigurationModel,
