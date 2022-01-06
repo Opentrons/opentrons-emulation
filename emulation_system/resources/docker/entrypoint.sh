03811bd575e98c86a55553417eefb22f907535b3
Derek Maggio <DerekMaggio@users.noreply.github.com>
2022-01-06 09:41:40 -0800

    chore: Update Docker Resources (#46)
    entrypoint.sh
    Append either `hardware` or `firmware` to build and run commands
    Add python building steps to firmware build commands
    Add error handling if command is not found
    
    Dockerfile
    Rename some of the OPENTRONS_HARDWARE env vars
    Pull firmware-builder build commands and add it to entrypoint.sh
    
    Compose Files
    Add networks
    Update names
    Remove ports from proxy as it is using a network

--- emulation_system/resources/docker/entrypoint.sh
+++ emulation_system/resources/docker/entrypoint.sh
@@ -19,51 +19,63 @@ fi
 
 FULL_COMMAND="$COMMAND"-"$OPENTRONS_HARDWARE"
 OTHER_ARGS=`echo "${@:2}"`
-
+echo "${FULL_COMMAND}"
 case $FULL_COMMAND in
-  build-heater-shaker)
+  build-heater-shaker-hardware)
     (
       cd /opentrons-modules && \
       cmake --preset=stm32-host-gcc10 . && \
       cmake --build ./build-stm32-host --target heater-shaker-simulator
     )
     ;;
-  run-heater-shaker)
-    bash -c "/opentrons-modules/build-stm32-host/stm32-modules/heater-shaker/simulator/heater-shaker-simulator $OTHER_ARGS"
-    ;;
-  build-thermocycler)
-    (
-      cd /opentrons-modules && \
-      cmake --preset=stm32-host-gcc10 . && \
-      cmake --build ./build-stm32-host --target thermocycler-refresh-simulator
-    )
-    ;;
-  run-thermocycler)
-    bash -c "/opentrons-modules/build-stm32-host/stm32-modules/thermocycler-refresh/simulator/thermocycler-refresh-simulator $OTHER_ARGS"
-    ;;
-  run-emulator-proxy)
-    python3 -m opentrons.hardware_control.emulation.app
-    ;;
-  run-thermocycler-driver)
-    bash -c "python3 -m opentrons.hardware_control.emulation.scripts.run_module_emulator thermocycler $OTHER_ARGS"
-    ;;
-  run-tempdeck-driver)
-    bash -c "python3 -m opentrons.hardware_control.emulation.scripts.run_module_emulator tempdeck $OTHER_ARGS"
-    ;;
-  run-magdeck-driver)
-    bash -c "python3 -m opentrons.hardware_control.emulation.scripts.run_module_emulator magdeck $OTHER_ARGS"
-    ;;
-  run-robot-server)
-    bash -c "uvicorn "robot_server:app" --host 0.0.0.0 --port 31950 --ws wsproto --reload"
-    ;;
-  build-ot3-firmware-echo)
+  build-ot3-echo-hardware)
     (
       cd /ot3-firmware && \
       cmake --preset host-gcc10 && \
       cmake --build ./build-host --target can-simulator
     )
     ;;
-  run-ot3-firmware-echo)
+  build-thermocycler-hardware)
+    (
+      cd /opentrons-modules && \
+      cmake --preset=stm32-host-gcc10 . && \
+      cmake --build ./build-stm32-host --target thermocycler-refresh-simulator
+    )
+    ;;
+  build-thermocycler-firmware|build-magdeck-firmware|build-tempdeck-firmware|build-emulator-proxy|build-robot-server|build-common-firmware)
+    (cd /opentrons/shared-data/python && python3 setup.py bdist_wheel -d /dist/)
+    (cd /opentrons/api && python3 setup.py bdist_wheel -d /dist/)
+    (cd /opentrons/notify-server && python3 setup.py bdist_wheel -d /dist/)
+    (cd /opentrons/robot-server && python3 setup.py bdist_wheel -d /dist/)
+    pip install /dist/*
+    ;;
+  run-heater-shaker-hardware)
+    bash -c "/opentrons-modules/build-stm32-host/stm32-modules/heater-shaker/simulator/heater-shaker-simulator $OTHER_ARGS"
+    ;;
+  run-thermocycler-hardware)
+    bash -c "/opentrons-modules/build-stm32-host/stm32-modules/thermocycler-refresh/simulator/thermocycler-refresh-simulator $OTHER_ARGS"
+    ;;
+  run-ot3-echo-hardware)
     /ot3-firmware/build-host/can/simulator/can-simulator
+    ;;
+  run-thermocycler-firmware)
+    bash -c "python3 -m opentrons.hardware_control.emulation.scripts.run_module_emulator thermocycler $OTHER_ARGS"
+    ;;
+  run-tempdeck-firmware)
+    bash -c "python3 -m opentrons.hardware_control.emulation.scripts.run_module_emulator tempdeck $OTHER_ARGS"
+    ;;
+  run-magdeck-firmware)
+    bash -c "python3 -m opentrons.hardware_control.emulation.scripts.run_module_emulator magdeck $OTHER_ARGS"
+    ;;
+  run-robot-server)
+    bash -c "uvicorn "robot_server:app" --host 0.0.0.0 --port 31950 --ws wsproto --reload"
+    ;;
+  run-emulator-proxy)
+    python3 -m opentrons.hardware_control.emulation.app
+    ;;
+
+  *)
+    echo "Command ${FULL_COMMAND} not found."
+    exit 2
     ;;
 esac
