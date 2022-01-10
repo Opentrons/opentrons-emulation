f7e0348edca0e8fa00212f33b162fb744b5045db
Derek Maggio <derek.maggio@opentrons.com>
2022-01-10 11:20:02 -0800

    feat: Parse services
    Parse individual containers in Services for Docker Compose file.

--- emulation_system/emulation_system/compose_file_creator/conversion_layer.py
+++ emulation_system/emulation_system/compose_file_creator/conversion_layer.py
@@ -1,11 +1,25 @@
 """Module to build Docker Compose file."""
 from __future__ import annotations
+
+from typing import (
+    List,
+    Union,
+    cast,
+)
+
 from emulation_system.compose_file_creator.input.configuration_file import (
     SystemConfigurationModel,
+)
+from emulation_system.compose_file_creator.output.compose_file_model import (
+    BuildItem,
+    Service,
+    Volume1,
 )
 from emulation_system.compose_file_creator.output.runtime_compose_file_model import (
     RuntimeComposeFileModel,
 )
+from emulation_system.compose_file_creator.settings.custom_types import Containers
+from emulation_system.consts import DOCKERFILE_DIR_LOCATION
 
 
 class ConversionLayer:
@@ -19,10 +33,32 @@ class ConversionLayer:
         self.compose_model = RuntimeComposeFileModel()
 
         self._set_version()
+        self._create_services()
 
     def _set_version(self) -> None:
         """Sets version on Compose file."""
         self.compose_model.version = self._config_model.compose_file_version
+
+    @staticmethod
+    def _configure_service(container: Containers) -> Service:
+        """Configure and return an individual service."""
+        service = Service()
+        service.container_name = container.id
+        service.tty = True
+        service.image = f"{container.get_image_name()}:latest"
+        service.build = BuildItem(
+            context=DOCKERFILE_DIR_LOCATION, target=container.get_image_name()
+        )
+
+        service.volumes = cast(List[Union[str, Volume1]], container.get_mount_strings())
+        return service
+
+    def _create_services(self) -> None:
+        """Define services for compose file."""
+        self.compose_model.services = {
+            container.id: self._configure_service(container)
+            for container in self._config_model.containers.values()
+        }
 
     def add_ports(self) -> None:
         """Adds ports to compose file."""
@@ -47,8 +83,4 @@ class ConversionLayer:
 
     def add_build_args(self) -> None:
         """Adds build arguments to compose file."""
-        pass
-
-    def add_bind_mounts(self) -> None:
-        """Adds bind mounts to compose file."""
         pass
