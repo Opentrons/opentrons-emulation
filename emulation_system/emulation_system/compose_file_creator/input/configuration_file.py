79619ee278ae447ce225c19c86c0d14e4ba38464
Derek Maggio <derek.maggio@opentrons.com>
2022-01-11 09:50:46 -0800

    Add proxy source override

--- emulation_system/emulation_system/compose_file_creator/input/configuration_file.py
+++ emulation_system/emulation_system/compose_file_creator/input/configuration_file.py
@@ -43,6 +43,9 @@ class SystemConfigurationModel(BaseModel):
     system_network_name: Optional[str] = Field(
         alias="system-network-name", regex=r"^[A-Za-z0-9-]+$"
     )
+    use_local_source_for_emulator_proxy: Optional[bool] = Field(
+        alias="use-local-source-for-emulator-proxy"
+    )
     robot: Optional[Robots]
     modules: Optional[List[Modules]] = Field(default=[])
 
@@ -104,6 +107,11 @@ class SystemConfigurationModel(BaseModel):
     def set_default_network_name(cls, v: str) -> str:
         """Sets default network name if nothing is specified."""
         return v or DEFAULT_NETWORK_NAME
+
+    @validator("use_local_source_for_emulator_proxy", pre=True, always=True)
+    def set_default_proxy_source(cls, v: bool) -> bool:
+        """Sets default network name if nothing is specified."""
+        return v or False
 
     @property
     def modules_exist(self) -> bool:
