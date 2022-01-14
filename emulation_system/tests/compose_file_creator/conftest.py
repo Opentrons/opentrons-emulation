603b4059d31d8558f079b5015e118ed6e1272344
Derek Maggio <derek.maggio@opentrons.com>
2022-01-14 07:51:02 -0800

    Add OT2

--- emulation_system/tests/compose_file_creator/conftest.py
+++ emulation_system/tests/compose_file_creator/conftest.py
@@ -1,5 +1,5 @@
 """Conftest for compose_file_creator package."""
-
+import pathlib
 from typing import (
     Any,
     Dict,
@@ -19,18 +19,21 @@ HEATER_SHAKER_MODULE_ID = "my-heater-shaker"
 MAGNETIC_MODULE_ID = "my-magnetic"
 TEMPERATURE_MODULE_ID = "my-temperature"
 THERMOCYCLER_MODULE_ID = "my-thermocycler"
+OT2_ID = "my-ot2"
 
 
 HEATER_SHAKER_MODULE_EMULATION_LEVEL = EmulationLevels.HARDWARE.value
 MAGNETIC_MODULE_EMULATION_LEVEL = EmulationLevels.FIRMWARE.value
 TEMPERATURE_MODULE_EMULATION_LEVEL = EmulationLevels.FIRMWARE.value
 THERMOCYCLER_MODULE_EMULATION_LEVEL = EmulationLevels.HARDWARE.value
+OT2_EMULATION_LEVEL = EmulationLevels.FIRMWARE.value
 
 
 HEATER_SHAKER_MODULE_SOURCE_TYPE = SourceType.LOCAL.value
 MAGNETIC_MODULE_SOURCE_TYPE = SourceType.LOCAL.value
 TEMPERATURE_MODULE_SOURCE_TYPE = SourceType.LOCAL.value
 THREMOCYCLER_MODULE_SOURCE_TYPE = SourceType.LOCAL.value
+OT2_SOURCE_TYPE = SourceType.LOCAL.value
 
 
 @pytest.fixture
@@ -167,3 +170,64 @@ def thermocycler_module_hardware_emulation_level(
     """Return heater-shaker configuration with an invalid emulation level."""
     thermocycler_module_default["emulation-level"] = EmulationLevels.HARDWARE.value
     return thermocycler_module_default
+
+
+@pytest.fixture
+def ot2_default(tmpdir: py.path.local) -> Dict[str, Any]:
+    """OT-2 using default pipettes."""
+    return {
+        "id": OT2_ID,
+        "hardware": Hardware.OT2.value,
+        "emulation-level": OT2_EMULATION_LEVEL,
+        "source-type": OT2_SOURCE_TYPE,
+        "source-location": str(tmpdir),
+        "hardware-specific-attributes": {},
+    }
+
+
+@pytest.fixture
+def ot2_bad_emulation_level(ot2_default: Dict[str, Any]) -> Dict[str, Any]:
+    """Return magnetic module configuration with an invalid emulation level."""
+    ot2_default["emulation-level"] = EmulationLevels.HARDWARE.value
+    return ot2_default
+
+
+@pytest.fixture
+def ot2_with_pipettes(ot2_default: Dict[str, Any]) -> Dict[str, Any]:
+    """OT-2 using user-specified pipettes."""
+    ot2_default["hardware-specific-attributes"]["left-pipette"] = {}
+    ot2_default["hardware-specific-attributes"]["left-pipette"]["model"] = "test_1"
+    ot2_default["hardware-specific-attributes"]["left-pipette"]["id"] = "test_1_id"
+
+    ot2_default["hardware-specific-attributes"]["right-pipette"] = {}
+    ot2_default["hardware-specific-attributes"]["right-pipette"]["model"] = "test_2"
+    ot2_default["hardware-specific-attributes"]["right-pipette"]["id"] = "test_2_id"
+    return ot2_default
+
+
+@pytest.fixture
+def ot2_with_mounts(tmp_path: pathlib.Path, ot2_default: Dict) -> Dict:
+    """Configuration of a robot with extra bind mounts."""
+    datadog_dir = tmp_path / "Datadog"
+    datadog_dir.mkdir()
+    datadog_file = datadog_dir / "log.txt"
+    datadog_file.write_text("test")
+
+    log_dir = tmp_path / "Log"
+    log_dir.mkdir()
+
+    ot2_default["robot"][OT2_ID]["extra-mounts"] = [
+        {
+            "name": "DATADOG",
+            "source-path": str(datadog_file),
+            "mount-path": "/datadog/log.txt",
+            "type": "file",
+        },
+        {
+            "name": "LOG_FILES",
+            "source-path": str(log_dir),
+            "mount-path": "/var/log/opentrons/",
+            "type": "directory",
+        },
+    ]
+    return ot2_default
