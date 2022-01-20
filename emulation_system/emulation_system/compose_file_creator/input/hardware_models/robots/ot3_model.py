991800a1d88a3959c87d26ed39dcb5bc87f59907
Derek Maggio <derek.maggio@opentrons.com>
2022-01-20 03:56:43 -0800

    Add OT3 model

---
+++ emulation_system/emulation_system/compose_file_creator/input/hardware_models/robots/ot3_model.py
@@ -0,0 +1,51 @@
+"""OT-3 Module and it's attributes."""
+from pydantic import Field
+from typing_extensions import Literal
+
+from emulation_system.compose_file_creator.input.hardware_models.hardware_specific_attributes import (  # noqa: E501
+    HardwareSpecificAttributes,
+)
+from emulation_system.compose_file_creator.input.hardware_models.robots.robot_model import (  # noqa: E501
+    RobotInputModel,
+)
+from emulation_system.compose_file_creator.settings.config_file_settings import (
+    EmulationLevels,
+    Hardware,
+    OpentronsRepository,
+    PipetteSettings,
+    SourceRepositories,
+)
+
+
+class OT3Attributes(HardwareSpecificAttributes):
+    """Attributes specific to OT3."""
+
+    left_pipette: PipetteSettings = Field(
+        alias="left-pipette", default=PipetteSettings()
+    )
+    right_pipette: PipetteSettings = Field(
+        alias="right-pipette", default=PipetteSettings()
+    )
+
+
+class OT3SourceRepositories(SourceRepositories):
+    """Source repositories for OT3."""
+
+    firmware_repo_name: OpentronsRepository = OpentronsRepository.OPENTRONS
+    hardware_repo_name: Literal[None] = None
+
+
+class OT3InputModel(RobotInputModel):
+    """Model for OT3."""
+
+    hardware: Literal[Hardware.OT3]
+    source_repos: OT3SourceRepositories = Field(
+        default=OT3SourceRepositories(), const=True
+    )
+    hardware_specific_attributes: OT3Attributes = Field(
+        alias="hardware-specific-attributes", default=OT3Attributes()
+    )
+    emulation_level: Literal[
+        EmulationLevels.FIRMWARE, EmulationLevels.HARDWARE
+    ] = Field(alias="emulation-level")
+    bound_port: int = Field(alias="bound-port", default=31950)
