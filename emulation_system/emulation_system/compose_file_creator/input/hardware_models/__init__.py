2f7ba2ab72c7060df5549406b292a698a8e06b45
Derek Maggio <derek.maggio@opentrons.com>
2022-01-20 07:26:04 -0800

    Change to relative paths

--- emulation_system/emulation_system/compose_file_creator/input/hardware_models/__init__.py
+++ emulation_system/emulation_system/compose_file_creator/input/hardware_models/__init__.py
@@ -1,28 +1,12 @@
 """hardware_models package."""
-from emulation_system.compose_file_creator.input.hardware_models.modules.heater_shaker_module import (  # noqa: E501
-    HeaterShakerModuleInputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.modules.magnetic_module import (  # noqa: E501
-    MagneticModuleInputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.modules.module_model import (  # noqa: E501
-    ModuleInputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.modules.temperature_module import (  # noqa: E501
-    TemperatureModuleInputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.modules.thermocycler_module import (  # noqa: E501
-    ThermocyclerModuleInputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.robots.ot2_model import (  # noqa: E501
-    OT2InputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.robots.ot3_model import (  # noqa: E501
-    OT3InputModel,
-)
-from emulation_system.compose_file_creator.input.hardware_models.robots.robot_model import (  # noqa: E501
-    RobotInputModel,
-)
+from .modules.heater_shaker_module import HeaterShakerModuleInputModel
+from .modules.magnetic_module import MagneticModuleInputModel
+from .modules.module_model import ModuleInputModel
+from .modules.temperature_module import TemperatureModuleInputModel
+from .modules.thermocycler_module import ThermocyclerModuleInputModel
+from .robots.ot2_model import OT2InputModel
+from .robots.ot3_model import OT3InputModel
+from .robots.robot_model import RobotInputModel
 
 __all__ = [
     "OT2InputModel",
