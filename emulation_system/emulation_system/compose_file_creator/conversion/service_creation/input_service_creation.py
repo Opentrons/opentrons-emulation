ca7f00c3804816f491abe9d6362128065c213dc2
Derek Maggio <derek.maggio@opentrons.com>
2022-01-26 13:59:05 -0800

    Change env var logic for input type services
    Switch OT_SMOOTHIE_EMULATOR_URI env var from being defined on any instance of robot serve to only being defined on OT2s
    Change OT_SMOOTHIE_EMULATOR_URI from being bound to emulator-proxy service  and instead bind it to smoothie service

--- emulation_system/emulation_system/compose_file_creator/conversion/service_creation/input_service_creation.py
+++ emulation_system/emulation_system/compose_file_creator/conversion/service_creation/input_service_creation.py
@@ -6,7 +6,6 @@ from typing import (
     List,
     Optional,
     Union,
-    cast,
 )
 
 from emulation_system.compose_file_creator.conversion.intermediate_types import (
@@ -23,6 +22,7 @@ from emulation_system.compose_file_creator.input.configuration_file import (
 )
 from emulation_system.compose_file_creator.input.hardware_models import (
     ModuleInputModel,
+    OT2InputModel,
     OT3InputModel,
     RobotInputModel,
 )
@@ -30,7 +30,6 @@ from emulation_system.compose_file_creator.output.compose_file_model import (
     ListOrDict,
     Port,
     Service,
-    Volume1,
 )
 from emulation_system.compose_file_creator.settings.custom_types import Containers
 
@@ -72,24 +71,30 @@ def _get_port_bindings(
 
 
 def _get_env_vars(
-    container: Containers, emulator_proxy_name: Optional[str]
+    container: Containers,
+    emulator_proxy_name: Optional[str],
+    smoothie_name: Optional[str],
 ) -> ListOrDict:
     temp_vars: Dict[str, Any] = {}
+
+    container_class = container.__class__
     if (
-        issubclass(container.__class__, RobotInputModel)
+        issubclass(container_class, RobotInputModel)
         and emulator_proxy_name is not None
     ):
+        temp_vars["OT_EMULATOR_module_server"] = f'{{"host": "{emulator_proxy_name}"}}'
+
+    if issubclass(container_class, OT2InputModel):
         # TODO: If emulator proxy port is ever not hardcoded will have to update from
         #  11000 to a variable
-        temp_vars["OT_SMOOTHIE_EMULATOR_URI"] = f"socket://{emulator_proxy_name}:11000"
-        temp_vars["OT_EMULATOR_module_server"] = f'{{"host": "{emulator_proxy_name}"}}'
-    elif issubclass(container.__class__, OT3InputModel):
+        temp_vars["OT_SMOOTHIE_EMULATOR_URI"] = f"socket://{smoothie_name}:11000"
+
+    if issubclass(container_class, OT3InputModel):
         temp_vars["OT_API_FF_enableOT3HardwareController"] = True
-    elif issubclass(container.__class__, ModuleInputModel):
+
+    if issubclass(container_class, ModuleInputModel):
         temp_vars.update(container.get_serial_number_env_var())
         temp_vars.update(container.get_proxy_info_env_var())
-    else:
-        temp_vars = {}
 
     return ListOrDict(__root__=temp_vars)
 
@@ -97,6 +102,7 @@ def _get_env_vars(
 def configure_input_service(
     container: Containers,
     emulator_proxy_name: Optional[str],
+    smoothie_name: Optional[str],
     config_model: SystemConfigurationModel,
     required_networks: RequiredNetworks,
 ) -> Service:
@@ -111,6 +117,6 @@ def configure_input_service(
         depends_on=_get_service_depends_on(container, emulator_proxy_name),
         ports=_get_port_bindings(container),
         command=_get_command(container, emulator_proxy_name),
-        environment=_get_env_vars(container, emulator_proxy_name),
+        environment=_get_env_vars(container, emulator_proxy_name, smoothie_name),
     )
     return service
