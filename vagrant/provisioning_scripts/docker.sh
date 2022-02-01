301f121b81656a7d4e6744a1a3d52bd937154c0c
Derek Maggio <derek.maggio@opentrons.com>
2022-02-01 12:14:55 -0800

    Move shell script files

--- vagrant/provisioning_scripts/docker.sh
+++ vagrant/provisioning_scripts/docker.sh
@@ -3,7 +3,7 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo \
    "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
@@ -11,8 +11,8 @@ groupadd docker
 usermod -a -G docker vagrant
 
 # Install Docker-Compose
- curl \
-   -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
-   -o /usr/local/bin/docker-compose
+curl \
+  -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" \
+  -o /usr/local/bin/docker-compose
 
  chmod +x /usr/local/bin/docker-compose
