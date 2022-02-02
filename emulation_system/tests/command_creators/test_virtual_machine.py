6f6536ec748c84a4449bc58ccd08284c0a32810b
Derek Maggio <derek.maggio@opentrons.com>
2022-02-02 06:49:08 -0800

    Fix test

--- emulation_system/tests/command_creators/test_virtual_machine.py
+++ emulation_system/tests/command_creators/test_virtual_machine.py
@@ -26,33 +26,31 @@ def test_get_virtual_machine_config(test_vm_command: VirtualMachineCommand) -> N
     assert settings.VM_MEMORY == 4096
     assert settings.VM_CPUS == 2
     assert settings.NUM_SOCKET_CAN_NETWORKS == "2"
-    assert settings.SHARED_FOLDERS == [
-        parse_obj_as(
-            SharedFolder,
-            {
-                "host-path": "/home/Documents/repos/opentrons",
-                "vm-path": "/home/vagrant/opentrons",
-            },
-        ),
-        parse_obj_as(
-            SharedFolder,
-            {
-                "host-path": "/home/Documents/repos/ot3-firmware",
-                "vm-path": "/home/vagrant/ot3-firmware",
-            },
-        ),
-        parse_obj_as(
-            SharedFolder,
-            {
-                "host-path": "/home/Documents/repos/opentrons-modules",
-                "vm-path": "/home/vagrant/opentrons-modules",
-            },
-        ),
-        parse_obj_as(
-            SharedFolder,
-            {
-                "host-path": "/home/derek-maggio/Documents/repos/opentrons-emulation",
-                "vm-path": "/home/vagrant/opentrons-emulation",
-            },
-        ),
-    ]
+    opentrons_folder = parse_obj_as(
+        SharedFolder,
+        {
+            "host-path": "/home/Documents/repos/opentrons",
+            "vm-path": "/home/vagrant/opentrons",
+        },
+    )
+    ot3_folder = parse_obj_as(
+        SharedFolder,
+        {
+            "host-path": "/home/Documents/repos/ot3-firmware",
+            "vm-path": "/home/vagrant/ot3-firmware",
+        },
+    )
+    opentrons_modules_folder = parse_obj_as(
+        SharedFolder,
+        {
+            "host-path": "/home/Documents/repos/opentrons-modules",
+            "vm-path": "/home/vagrant/opentrons-modules",
+        },
+    )
+    assert opentrons_folder in settings.SHARED_FOLDERS
+    assert ot3_folder in settings.SHARED_FOLDERS
+    assert opentrons_modules_folder in settings.SHARED_FOLDERS
+
+    for folder in settings.SHARED_FOLDERS:
+        if "opentrons-emulation" in folder.host_path:
+            assert folder.vm_path == "/home/vagrant/opentrons-emulation"
