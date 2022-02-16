dc8b0154439069cf761f030d13c43c5b2992e18a
Derek Maggio <DerekMaggio@users.noreply.github.com>
2022-02-16 13:17:54 -0800

    chore: Configuration for cpx (#84)
    * feat: Create configuration for CPX
    
    * Make robot server use robot-server-type when getting image name
    
    Override get_image_name method in RobotInputModel 
    When creating a robot server will call overridden version of get_image_name.
    This overridden version using robot_server_source_type instead of source_type
    
    * Fix entrypoint
    
    Remove extra build command
    Remove previously built packages on build
    Run uvicorn without reload, because reload is annoying
    
    * Add setup README

--- emulation_system/emulation_system/compose_file_creator/input/hardware_models/robots/robot_model.py
+++ emulation_system/emulation_system/compose_file_creator/input/hardware_models/robots/robot_model.py
@@ -24,6 +24,7 @@ from emulation_system.compose_file_creator.settings.config_file_settings import 
     ROBOT_SERVER_MOUNT_NAME,
     SourceType,
 )
+from emulation_system.compose_file_creator.settings.images import get_image_name
 
 
 class RobotInputModel(HardwareModel):
@@ -63,3 +64,9 @@ class RobotInputModel(HardwareModel):
     def get_port_binding_string(self) -> str:
         """Get port binding string for Docker Compose file."""
         return f"{self.exposed_port}:{self.bound_port}"
+
+    def get_image_name(self) -> str:
+        """Get image name to run based off of passed parameters."""
+        return get_image_name(
+            self.hardware, self.robot_server_source_type, self.emulation_level
+        )
