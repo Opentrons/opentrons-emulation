919ffe0b5e2e2fc9bdb1a37887561eb737abf1b4
Derek Maggio <DerekMaggio@users.noreply.github.com>
2022-02-17 07:56:45 -0800

    chore: Remove unused functionality (#89)
    * Pull out all VM garbage that is making my life difficult
    
    * Clean up config files
    
    * Remove all vagrant stuff
    
    Not going to use it because it is a PITA

--- emulation_system/emulation_system/opentrons_emulation_configuration.py
+++ emulation_system/emulation_system/opentrons_emulation_configuration.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import json
 import os
-from typing import Any, Dict, List, Optional
 
 from pydantic import BaseModel, Field, parse_obj_as
 
@@ -22,20 +21,6 @@ class ConfigurationFileNotFoundError(FileNotFoundError):
     """Error thrown when config file is not found."""
 
     pass
-
-
-class DefaultFolderPaths(BaseModel):
-    """Default folder paths to find repos at."""
-
-    opentrons: Optional[str]
-    ot3_firmware: Optional[str] = Field(alias="ot3-firmware")
-    modules: Optional[str]
-
-
-class GlobalSettings(BaseModel):
-    """Settings that affect all sections in config file."""
-
-    default_folder_paths: DefaultFolderPaths = Field(..., alias="default-folder-paths")
 
 
 class Heads(BaseModel):
@@ -69,31 +54,10 @@ class EmulationSettings(BaseModel):
     )
 
 
-class SharedFolder(BaseModel):
-    """Model for a shared folder. Will map host_path to vm_path."""
-
-    host_path: str = Field(..., alias="host-path")
-    vm_path: str = Field(..., alias="vm-path")
-
-
-class VirtualMachineSettings(BaseModel):
-    """All settings related to vm command."""
-
-    vm_memory: int = Field(..., alias="vm-memory")
-    vm_cpus: int = Field(..., alias="vm-cpus")
-    num_socket_can_networks: int = Field(..., alias="num-socket-can-networks")
-    shared_folders: List[str] = Field(alias="shared-folders", default=[])
-
-
 class OpentronsEmulationConfiguration(BaseModel):
     """Model representing entire configuration file."""
 
-    global_settings: GlobalSettings = Field(..., alias="global-settings")
     emulation_settings: EmulationSettings = Field(..., alias="emulation-settings")
-    virtual_machine_settings: VirtualMachineSettings = Field(
-        ..., alias="virtual-machine-settings"
-    )
-    aws_ecr_settings: Dict[str, Any] = Field(..., alias="aws-ecr-settings")
 
     @classmethod
     def from_file_path(cls, json_file_path: str) -> OpentronsEmulationConfiguration:
