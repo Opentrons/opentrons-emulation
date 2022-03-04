829c1ef9d2ddfc3b18039d04015c5e6f436f3601
Derek Maggio <derek.maggio@opentrons.com>
2022-03-04 09:44:36 -0800

    Test yaml substitution works

---
+++ emulation_system/tests/compose_file_creator/utilities/test_substitute_yaml_values.py
@@ -0,0 +1,49 @@
+"""Test that yaml substitution works."""
+
+import pytest
+import yaml
+
+from emulation_system.compose_file_creator.utilities.substitute_yaml_values import (
+    Substitution,
+    YamlSubstitution,
+)
+
+SUB_LIST = [
+    Substitution("otie", "source-location", "48038c4d189536a0862a2c20ed832dc34bd1c8b2"),
+    Substitution("otie", "exposed-port", "5000"),
+]
+
+
+@pytest.fixture
+def remote_only_ot3() -> str:
+    """Remote only OT-3 for testing."""
+    return yaml.dump(
+        {
+            "robot": {
+                "id": "otie",
+                "hardware": "ot3",
+                "emulation-level": "hardware",
+                "source-type": "remote",
+                "source-location": "latest",
+                "robot-server-source-type": "remote",
+                "robot-server-source-location": "latest",
+                "can-server-source-type": "remote",
+                "can-server-source-location": "latest",
+                "exposed-port": 31950,
+                "hardware-specific-attributes": {},
+            }
+        }
+    )
+
+
+def test_yaml_substitution(remote_only_ot3: str) -> None:
+    """Confirm that yaml substitutions work correctly."""
+    resultant_config_model = YamlSubstitution(
+        remote_only_ot3, SUB_LIST
+    ).perform_substitution()
+    assert resultant_config_model.robot is not None
+    assert (
+        resultant_config_model.robot.source_location
+        == "48038c4d189536a0862a2c20ed832dc34bd1c8b2"
+    )
+    assert resultant_config_model.robot.exposed_port == 5000
