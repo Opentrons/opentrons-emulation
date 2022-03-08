f90f82cc53e7ed66d80be06bba4d4e2c3732f8d0
Derek Maggio <derek.maggio@opentrons.com>
2022-03-08 11:42:37 -0800

    Add some comments

--- scripts/rebuild_and_run_local_containers.sh
+++ scripts/rebuild_and_run_local_containers.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-CONTAINER_IDS=`docker-compose -f - ps -q`
+CONTAINER_IDS=`docker-compose -f - ps -q`  # Get ids from generated compose file
+
 for container_id in ${CONTAINER_IDS}; do
+  # Load mounts from each container and check to see if they have entrypoint.sh mounted
   docker container inspect --format '{{ .Mounts }}' ${container_id} | grep -q "entrypoint.sh"
+
+  # If they have it mounted (grep exited with 0) stop, build, then run
   if [[ $? -eq 0 ]]; then
     docker exec ${container_id} bash -c "/entrypoint.sh stop"
     docker exec ${container_id} bash -c "/entrypoint.sh build" > /dev/null
