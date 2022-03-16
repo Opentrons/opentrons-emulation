a1d72f07c129f34f3730b937245f4c2e8d97d309
Derek Maggio <derek.maggio@opentrons.com>
2022-03-16 08:43:39 -0700

    Fix build command to only check for arm64.

--- scripts/makefile/helper_scripts/build.sh
+++ scripts/makefile/helper_scripts/build.sh
@@ -4,11 +4,9 @@ ARCH=`arch`
 
 FILE_PATH=$1
 
-if [ "${ARCH}" == "x86_64" ]; then
-  docker buildx bake --file ${FILE_PATH}
-elif [ "${ARCH}" == "arm64" ]; then
+
+if [ "${ARCH}" == "arm64" ]; then
   docker buildx bake --file ${FILE_PATH} --set *.platform=linux/x86_64
 else
-  echo "${ARCH} architecture not supported."
-  exit 1
+  docker buildx bake --file ${FILE_PATH}
 fi
