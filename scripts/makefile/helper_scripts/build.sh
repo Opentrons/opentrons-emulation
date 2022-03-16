c41597026f56280ab61f413092fcdad6c721d286
Derek Maggio <derek.maggio@opentrons.com>
2022-03-16 09:31:42 -0700

    A comment

--- scripts/makefile/helper_scripts/build.sh
+++ scripts/makefile/helper_scripts/build.sh
@@ -6,6 +6,9 @@ FILE_PATH=$1
 
 
 if [ "${ARCH}" == "arm64" ]; then
+  # Platform is being set to linux/x86_64 because a requirement of M1 Mac is to use rosetta
+  # Rosetta emulates a x86_64 architecture and then Docker uses that
+  # Docker thinks it should be trying to build against an arm64 arch. So we have to override it.
   docker buildx bake --file ${FILE_PATH} --set *.platform=linux/x86_64
 else
   docker buildx bake --file ${FILE_PATH}
