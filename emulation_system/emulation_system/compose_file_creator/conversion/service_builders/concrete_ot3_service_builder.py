990837dcf2de98bf45088da957683d2627c29a4f
Derek Maggio <derek.maggio@opentrons.com>
2022-09-15 12:55:14 -0700

    Refactor OT-3 Service Creation

---
+++ emulation_system/emulation_system/compose_file_creator/conversion/service_builders/concrete_ot3_service_builder.py
@@ -0,0 +1,132 @@
+"""Module containing ConcreteOT3ServiceBuilder class."""
+from typing import Optional
+
+from emulation_system import OpentronsEmulationConfiguration, SystemConfigurationModel
+from emulation_system.compose_file_creator import BuildItem
+from emulation_system.compose_file_creator.config_file_settings import (
+    OpentronsRepository,
+    SourceType,
+)
+from emulation_system.compose_file_creator.conversion.service_creation.shared_functions import (
+    add_ot3_firmware_named_volumes,
+    get_build_args,
+    get_entrypoint_mount_string,
+    get_service_build,
+)
+from emulation_system.compose_file_creator.types.intermediate_types import (
+    IntermediateCommand,
+    IntermediateDependsOn,
+    IntermediateEnvironmentVariables,
+    IntermediateNetworks,
+    IntermediatePorts,
+    IntermediateVolumes,
+)
+
+from .abstract_service_builder import AbstractServiceBuilder
+from .service_info import ServiceInfo
+
+
+class ConcreteOT3ServiceBuilder(AbstractServiceBuilder):
+    """Concrete implementation of AbstractServiceBuilder for building OT-3 firmware services."""
+
+    def __init__(
+        self,
+        config_model: SystemConfigurationModel,
+        global_settings: OpentronsEmulationConfiguration,
+        dev: bool,
+        can_server_service_name: str,
+        service_info: ServiceInfo,
+    ) -> None:
+        """Instantiates a ConcreteOT3ServiceBuilder object."""
+        super().__init__(config_model, global_settings)
+        self._dev = dev
+        self._can_server_service_name = can_server_service_name
+        self._service_info = service_info
+        self._ot3 = self.get_ot3(config_model)
+        self._image = self._generate_image()
+
+    def _generate_image(self) -> str:
+        """Inner method for generating image.
+
+        Using an inner method and setting the value to self._image so when other
+        methods need to access the image, this function is not called again.
+        This prevents, primarily, logging happening twice, but also the increased
+        overhead of calculating the same thing twice.
+        """
+        source_type = self._ot3.source_type
+        image_name = (
+            self._service_info.image.local_hardware_image_name
+            if source_type == SourceType.LOCAL
+            else self._service_info.image.remote_hardware_image_name
+        )
+        assert image_name is not None
+
+        return image_name
+
+    def generate_container_name(self) -> str:
+        """Generates value for container_name parameter."""
+        system_unique_id = self._config_model.system_unique_id
+        container_name = super()._generate_container_name(
+            self._service_info.container_name, system_unique_id
+        )
+
+        return container_name
+
+    def generate_image(self) -> str:
+        """Generates value for image parameter."""
+        return self._image
+
+    def is_tty(self) -> bool:
+        """Generates value for tty parameter."""
+        tty = True
+        return tty
+
+    def generate_networks(self) -> IntermediateNetworks:
+        """Generates value for networks parameter."""
+        networks = self._config_model.required_networks
+        return networks
+
+    def generate_build(self) -> Optional[BuildItem]:
+        """Generates value for build parameter."""
+        repo = OpentronsRepository.OT3_FIRMWARE
+        if self._ot3.source_type == SourceType.REMOTE:
+            build_args = get_build_args(
+                repo,
+                self._ot3.source_location,
+                self._global_settings.get_repo_commit(repo),
+                self._global_settings.get_repo_head(repo),
+            )
+
+        else:
+            build_args = None
+        return get_service_build(self._image, build_args, self._dev)
+
+    def generate_volumes(self) -> Optional[IntermediateVolumes]:
+        """Generates value for volumes parameter."""
+        if self._ot3.source_type == SourceType.LOCAL:
+            volumes = [get_entrypoint_mount_string()]
+            volumes.extend(self._ot3.get_mount_strings())
+            add_ot3_firmware_named_volumes(volumes)
+        else:
+            volumes = None
+        return volumes
+
+    def generate_command(self) -> Optional[IntermediateCommand]:
+        """Generates value for command parameter."""
+        command = None
+        return command
+
+    def generate_ports(self) -> Optional[IntermediatePorts]:
+        """Generates value for ports parameter."""
+        ports = self._ot3.get_can_server_bound_port()
+        return ports
+
+    def generate_depends_on(self) -> Optional[IntermediateDependsOn]:
+        """Generates value for depends_on parameter."""
+        depends_on = None
+        return depends_on
+
+    def generate_env_vars(self) -> Optional[IntermediateEnvironmentVariables]:
+        """Generates value for environment parameter."""
+        env_vars = {"CAN_SERVER_HOST": self._can_server_service_name}
+        return env_vars
--- emulation_system/emulation_system/compose_file_creator/conversion/service_builders/service_builder_orchestrator.py
+++ emulation_system/emulation_system/compose_file_creator/conversion/service_builders/service_builder_orchestrator.py
@@ -1,17 +1,38 @@
 """Module containing ServiceBuilderOrchestrator class."""
+from typing import List
 
 from emulation_system import OpentronsEmulationConfiguration, SystemConfigurationModel
 from emulation_system.compose_file_creator import Service
 
+from ...config_file_settings import OT3Hardware
+from ...images import (
+    OT3BootloaderImages,
+    OT3GantryXImages,
+    OT3GantryYImages,
+    OT3GripperImages,
+    OT3HeadImages,
+    OT3PipettesImages,
+)
 from . import (
     ConcreteCANServerServiceBuilder,
     ConcreteEmulatorProxyServiceBuilder,
+    ConcreteOT3ServiceBuilder,
     ConcreteSmoothieServiceBuilder,
 )
+from .service_info import ServiceInfo
 
 
 class ServiceBuilderOrchestrator:
     """Class that client uses to interface with builders."""
+
+    OT3_SERVICES_TO_CREATE = [
+        ServiceInfo(OT3HeadImages(), OT3Hardware.HEAD),
+        ServiceInfo(OT3PipettesImages(), OT3Hardware.PIPETTES),
+        ServiceInfo(OT3GantryXImages(), OT3Hardware.GANTRY_X),
+        ServiceInfo(OT3GantryYImages(), OT3Hardware.GANTRY_Y),
+        ServiceInfo(OT3BootloaderImages(), OT3Hardware.BOOTLOADER),
+        ServiceInfo(OT3GripperImages(), OT3Hardware.GRIPPER),
+    ]
 
     def __init__(
         self,
@@ -39,3 +60,18 @@ class ServiceBuilderOrchestrator:
         return ConcreteSmoothieServiceBuilder(
             self._config_model, self._global_settings, dev
         ).build_service()
+
+    def build_ot3_services(
+        self, dev: bool, can_server_service_name: str
+    ) -> List[Service]:
+        """Generates OT-3 Firmware Services."""
+        return [
+            ConcreteOT3ServiceBuilder(
+                self._config_model,
+                self._global_settings,
+                dev,
+                can_server_service_name,
+                service_info,
+            ).build_service()
+            for service_info in self.OT3_SERVICES_TO_CREATE
+        ]
