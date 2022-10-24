1a38af02630bcedcb9441171cec0221c77267b8c
Derek Maggio <derek.maggio@opentrons.com>
2022-10-24 11:23:51 -0700

    Build new bases. Hope this doesn't jack everything up

--- docker/build_bases.sh
+++ docker/build_bases.sh
@@ -11,11 +11,20 @@ BRANCH_NAME=$1
 if [[ "${BRANCH_NAME}" =~ release-v.* ]]; then
   docker buildx build \
   --file ./bases_Dockerfile \
-  --target ubuntu-base \
+  --target ubuntu-base-amd64 \
   --push \
-  --platform=linux/amd64,linux/arm64 \
-  --tag ghcr.io/opentrons/ubuntu-base:latest \
-  --tag ghcr.io/opentrons/ubuntu-base:${BRANCH_NAME} \
+  --platform=linux/amd64 \
+  --tag ghcr.io/opentrons/ubuntu-base-amd64:latest \
+  --tag ghcr.io/opentrons/ubuntu-base-amd64:${BRANCH_NAME} \
+  .
+
+  docker buildx build \
+  --file ./bases_Dockerfile \
+  --target ubuntu-base-arm64 \
+  --push \
+  --platform=linux/arm64 \
+  --tag ghcr.io/opentrons/ubuntu-base-arm64:latest \
+  --tag ghcr.io/opentrons/ubuntu-base-arm64:${BRANCH_NAME} \
   .
   docker buildx build \
   --file ./bases_Dockerfile \
@@ -34,22 +43,32 @@ if [[ "${BRANCH_NAME}" =~ release-v.* ]]; then
   --tag ghcr.io/opentrons/python-base:${BRANCH_NAME} \
   .
 else
+    docker buildx build \
+  --file ./bases_Dockerfile \
+  --target ubuntu-base-amd64 \
+  --push \
+  --platform=linux/amd64 \
+  --tag ghcr.io/opentrons/ubuntu-base-amd64:latest \
+  .
   docker buildx build \
   --file ./bases_Dockerfile \
-  --target ubuntu-base \
+  --target ubuntu-base-arm64 \
   --push \
-  --platform=linux/amd64,linux/arm64 \
-  --tag ghcr.io/opentrons/ubuntu-base:latest .
+  --platform=linux/arm64 \
+  --tag ghcr.io/opentrons/ubuntu-base-arm64:latest \
+  .
   docker buildx build \
   --file ./bases_Dockerfile \
   --target cpp-base \
   --push \
   --platform=linux/amd64,linux/arm64 \
-  --tag ghcr.io/opentrons/cpp-base:latest .
+  --tag ghcr.io/opentrons/cpp-base:latest \
+  .
   docker buildx build \
   --file ./bases_Dockerfile \
   --target python-base \
   --push \
   --platform=linux/amd64,linux/arm64 \
-  --tag ghcr.io/opentrons/python-base:latest .
+  --tag ghcr.io/opentrons/python-base:latest \
+  .
 fi
