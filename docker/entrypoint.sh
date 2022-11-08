f9532b326a911f27ddd07aecd40aae6db3fe5f41
Derek Maggio <derek.maggio@opentrons.com>
2022-11-08 09:37:46 -0800

    chore: Remove stop commands from entrypoint.sh (RQA-380)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,8 +8,8 @@ fi
 
 COMMAND=$1
 
-if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "run" ] && [ "$COMMAND" != "stop" ]; then
-  echo "Valid commands are \"build\", \"run\", and \"stop\""
+if [ "$COMMAND" != "build" ] && [ "$COMMAND" != "run" ]; then
+  echo "Valid commands are \"build\" and \"run\""
   echo "You passed $COMMAND"
   echo "Usage: $(basename "${BASH_SOURCE[0]}") <command>"
   exit 1
@@ -33,15 +33,6 @@ build_ot3_firmware_single_simulator() {
     cmake --build ./build-host -j $(expr $(nproc) - 1) --target $1
     )
 }
-
-kill_process() {
-  if [[ "$COMMAND" != "stop" ]]; then
-    echo "Cannot run kill_process outside of the stop command."
-    exit 1
-  fi
-  pkill -9 -f ${1:?error, must pass parameter to kill_process}
-}
-
 # OPENTRONS_HARDWARE is an env variable that is passed to every container
 
 FULL_COMMAND="$COMMAND"-"$OPENTRONS_HARDWARE"
@@ -137,10 +128,6 @@ case $FULL_COMMAND in
     )
     ;;
 
-  stop-ot3-gantry-y-hardware|stop-heater-shaker-hardware|stop-thermocycler-hardware|stop-ot3-pipettes-hardware|stop-ot3-head-hardware|stop-ot3-gantry-x-hardware)
-    kill_process $OPENTRONS_HARDWARE
-    ;;
-
   # Firmware Level
 
   build-thermocycler-firmware|build-heater-shaker-firmware|build-magdeck-firmware|build-tempdeck-firmware|build-emulator-proxy|build-robot-server|build-common-firmware|build-smoothie|build-can-server)
@@ -176,22 +163,6 @@ case $FULL_COMMAND in
     ;;
   run-can-server)
     bash -c "monorepo_python -m opentrons_hardware.scripts.sim_socket_can"
-    ;;
-
-  stop-thermocycler-firmware|stop-tempdeck-firmware|stop-magdeck-firmware)
-    kill_process opentrons.hardware_control.emulation.scripts.run_module_emulator
-    ;;
-  stop-emulator-proxy)
-    kill_process opentrons.hardware_control.emulation.app
-    ;;
-  stop-robot-server)
-    kill_process /usr/local/bin/uvicorn
-    ;;
-  stop-smoothie)
-    kill_process opentrons.hardware_control.emulation.script
-    ;;
-  stop-can-server)
-    kill_process opentrons_hardware.script
     ;;
 
   *)
