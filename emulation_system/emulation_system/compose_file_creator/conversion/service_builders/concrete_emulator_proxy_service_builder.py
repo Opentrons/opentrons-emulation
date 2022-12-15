5467e0b7e20d108821afb2ed4f44765dbc5b21fa
Derek Maggio <derek.maggio@opentrons.com>
2022-12-15 12:23:40 -0800

    Don't assert. Instead, stop sucking at my job, and move inside conditional

--- emulation_system/emulation_system/compose_file_creator/conversion/service_builders/concrete_emulator_proxy_service_builder.py
+++ emulation_system/emulation_system/compose_file_creator/conversion/service_builders/concrete_emulator_proxy_service_builder.py
@@ -10,7 +10,6 @@ from emulation_system.compose_file_creator.images import EmulatorProxyImages
 from emulation_system.compose_file_creator.input.hardware_models import (
     HeaterShakerModuleInputModel,
     MagneticModuleInputModel,
-    RobotInputModel,
     TemperatureModuleInputModel,
     ThermocyclerModuleInputModel,
 )
@@ -29,7 +28,7 @@ from emulation_system.compose_file_creator.utilities.shared_functions import (
 )
 
 from ...logging import EmulatorProxyLoggingClient
-from ...utilities.hardware_utils import is_ot3
+from ...utilities.hardware_utils import is_ot3, is_robot
 from .abstract_service_builder import AbstractServiceBuilder
 
 
@@ -156,12 +155,11 @@ class ConcreteEmulatorProxyServiceBuilder(AbstractServiceBuilder):
             for env_var_name, env_var_value in module.get_proxy_info_env_var().items()  # type: ignore [attr-defined]
         }
 
-        assert self._config_model.robot is not None
-        if is_ot3(self._config_model.robot):
-            env_vars["OPENTRONS_PROJECT"] = "ot3"
+        if self._config_model.robot is not None and is_robot(self._config_model.robot):
 
-        if self._config_model.robot is not None:
-            assert issubclass(self._config_model.robot.__class__, RobotInputModel)
+            if is_ot3(self._config_model.robot):
+                env_vars["OPENTRONS_PROJECT"] = "ot3"
+
             if self._config_model.robot.emulator_proxy_env_vars is not None:
                 env_vars.update(self._config_model.robot.emulator_proxy_env_vars)
 
