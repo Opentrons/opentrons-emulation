ce5d3ed5cd27d6eab254f8d5eb4b1e4795be46a7
Derek Maggio <derek.maggio@opentrons.com>
2023-02-22 09:03:40 -0800

    test: Rename and fix build args tests
    Use source objects now

---
+++ emulation_system/tests/compose_file_creator/conversion_logic/test_build_args.py
@@ -0,0 +1,74 @@
+"""Tests for conversion pure functions."""
+from typing import Dict, Type, Union
+
+import pytest
+from pytest_lazyfixture import lazy_fixture  # type: ignore[import]
+
+from emulation_system import OpentronsEmulationConfiguration
+from emulation_system.source import (
+    MonorepoSource,
+    OpentronsModulesSource,
+    OT3FirmwareSource,
+)
+from tests.conftest import FAKE_COMMIT_ID
+
+
+@pytest.mark.parametrize(
+    "source, source_location, expected_value",
+    [
+        [
+            MonorepoSource,
+            "latest",
+            {
+                "OPENTRONS_SOURCE_DOWNLOAD_LOCATION": "https://github.com/AnotherOrg/opentrons/archive/refs/heads/edge.zip"
+            },
+        ],
+        [
+            MonorepoSource,
+            FAKE_COMMIT_ID,
+            {
+                "OPENTRONS_SOURCE_DOWNLOAD_LOCATION": f"https://github.com/AnotherOrg/opentrons/archive/{FAKE_COMMIT_ID}.zip"
+            },
+        ],
+        [MonorepoSource, lazy_fixture("opentrons_dir"), None],
+        [
+            OpentronsModulesSource,
+            "latest",
+            {
+                "MODULE_SOURCE_DOWNLOAD_LOCATION": "https://github.com/AnotherOrg/opentrons-modules/archive/refs/heads/edge.zip"
+            },
+        ],
+        [
+            OpentronsModulesSource,
+            FAKE_COMMIT_ID,
+            {
+                "MODULE_SOURCE_DOWNLOAD_LOCATION": f"https://github.com/AnotherOrg/opentrons-modules/archive/{FAKE_COMMIT_ID}.zip"
+            },
+        ],
+        [OpentronsModulesSource, lazy_fixture("opentrons_modules_dir"), None],
+        [
+            OT3FirmwareSource,
+            "latest",
+            {
+                "FIRMWARE_SOURCE_DOWNLOAD_LOCATION": "https://github.com/AnotherOrg/ot3-firmware/archive/refs/heads/main.zip"
+            },
+        ],
+        [
+            OT3FirmwareSource,
+            FAKE_COMMIT_ID,
+            {
+                "FIRMWARE_SOURCE_DOWNLOAD_LOCATION": f"https://github.com/AnotherOrg/ot3-firmware/archive/{FAKE_COMMIT_ID}.zip"
+            },
+        ],
+        [OT3FirmwareSource, lazy_fixture("ot3_firmware_dir"), None],
+    ],
+)
+def test_get_build_args(
+    source: Type[Union[MonorepoSource, OpentronsModulesSource, OT3FirmwareSource]],
+    source_location: str,
+    expected_value: Dict[str, str] | None,
+    testing_global_em_config: OpentronsEmulationConfiguration,
+) -> None:
+    """Confirm that get_build_args is working as expected."""
+    source_obj = source(source_location=source_location)
+    assert source_obj.generate_build_args(testing_global_em_config) == expected_value
