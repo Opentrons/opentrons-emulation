81919d125ddfc7a36ec5521bc7343dee04d1a21d
Derek Maggio <derek.maggio@opentrons.com>
2023-02-22 08:12:17 -0800

    test: Fix OT3FirmwareBuilderService tests
    - Use new fixtures
    - Update image and container_name validation

--- emulation_system/tests/compose_file_creator/conversion_logic/service_builders/test_concrete_local_ot3_firmware_builder_builder.py
+++ emulation_system/tests/compose_file_creator/conversion_logic/service_builders/test_concrete_local_ot3_firmware_builder_builder.py
@@ -11,9 +11,9 @@ from emulation_system.compose_file_creator.config_file_settings import (
     RepoToBuildArgMapping,
 )
 from emulation_system.compose_file_creator.conversion.service_builders import (
-    ConcreteLocalOT3FirmwareBuilderBuilder,
+    OT3FirmwareBuilderService,
 )
-from tests.compose_file_creator.conversion_logic.conftest import (
+from tests.validation_helper_functions import (
     build_args_are_none,
     get_source_code_build_args,
 )
@@ -22,8 +22,8 @@ from tests.compose_file_creator.conversion_logic.conftest import (
 @pytest.mark.parametrize(
     "config_model",
     [
-        lazy_fixture("ot3_local_source"),
-        lazy_fixture("ot3_remote_source_local_opentrons_hardware"),
+        lazy_fixture("ot3_local_ot3_firmware_remote_monorepo"),
+        lazy_fixture("ot3_remote_ot3_firmware_local_monorepo"),
         lazy_fixture("ot3_local_everything"),
     ],
 )
@@ -33,14 +33,14 @@ def test_simple_values(
 ) -> None:
     """Test values common to all 3 configs."""
     model = parse_obj_as(SystemConfigurationModel, config_model)
-    service = ConcreteLocalOT3FirmwareBuilderBuilder(
+    service = OT3FirmwareBuilderService(
         model, testing_global_em_config, False
     ).build_service()
 
     assert service.image is not None
-    assert service.image == "local-ot3-firmware-builder"
+    assert service.image == "ot3-firmware-builder"
     assert service.container_name is not None
-    assert service.container_name == "local-ot3-firmware-builder"
+    assert service.container_name == "ot3-firmware-builder"
     assert service.tty
     assert isinstance(service.networks, list)
     assert len(service.networks) == 2
@@ -52,18 +52,21 @@ def test_simple_values(
 
 
 def test_local_ot3_firmware_remote_monorepo(
-    ot3_local_source: Dict[str, Any],
+    ot3_local_ot3_firmware_remote_monorepo: Dict[str, Any],
     opentrons_head: str,
     testing_global_em_config: OpentronsEmulationConfiguration,
 ) -> None:
     """Tests for when you are using local ot3-firmware and remote monorepo."""
-    model = parse_obj_as(SystemConfigurationModel, ot3_local_source)
-    service = ConcreteLocalOT3FirmwareBuilderBuilder(
+    model = parse_obj_as(
+        SystemConfigurationModel, ot3_local_ot3_firmware_remote_monorepo
+    )
+    service = OT3FirmwareBuilderService(
         model, testing_global_em_config, False
     ).build_service()
 
     monorepo_build_arg = RepoToBuildArgMapping.OPENTRONS
     build_args = get_source_code_build_args(service)
+    assert build_args is not None
     assert len(build_args) == 1
     assert monorepo_build_arg in build_args
     assert build_args[monorepo_build_arg] == opentrons_head
@@ -75,20 +78,21 @@ def test_local_ot3_firmware_remote_monorepo(
 
 
 def test_remote_ot3_firmware_local_monorepo(
-    ot3_remote_source_local_opentrons_hardware: Dict[str, Any],
+    ot3_remote_ot3_firmware_local_monorepo: Dict[str, Any],
     ot3_firmware_head: str,
     testing_global_em_config: OpentronsEmulationConfiguration,
 ) -> None:
     """Tests for when you are using remote ot3-firmware and local monorepo."""
     model = parse_obj_as(
-        SystemConfigurationModel, ot3_remote_source_local_opentrons_hardware
+        SystemConfigurationModel, ot3_remote_ot3_firmware_local_monorepo
     )
-    service = ConcreteLocalOT3FirmwareBuilderBuilder(
+    service = OT3FirmwareBuilderService(
         model, testing_global_em_config, False
     ).build_service()
 
     ot3_firmware_build_arg = RepoToBuildArgMapping.OT3_FIRMWARE
     build_args = get_source_code_build_args(service)
+    assert build_args is not None
     assert len(build_args) == 1
     assert ot3_firmware_build_arg in build_args
     assert build_args[ot3_firmware_build_arg] == ot3_firmware_head
@@ -106,7 +110,7 @@ def test_local_everything(
 ) -> None:
     """Tests for when you are using local ot3-firmware and local monorepo."""
     model = parse_obj_as(SystemConfigurationModel, ot3_local_everything)
-    service = ConcreteLocalOT3FirmwareBuilderBuilder(
+    service = OT3FirmwareBuilderService(
         model, testing_global_em_config, False
     ).build_service()
 
