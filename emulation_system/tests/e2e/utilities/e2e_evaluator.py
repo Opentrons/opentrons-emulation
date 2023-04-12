9c42fddbf0ea9c235d5f2d4a7e9b6becb42618bd
Derek Maggio <derek.maggio@opentrons.com>
2023-04-12 12:10:58 -0700

    Add OT3 Emulator Named volume check

--- emulation_system/tests/e2e/utilities/e2e_evaluator.py
+++ emulation_system/tests/e2e/utilities/e2e_evaluator.py
@@ -1,10 +1,13 @@
 from tests.e2e.fixtures.ot3_containers import OT3SystemUnderTest
+from tests.e2e.utilities.consts import OT3FirmwareEmulatorNamedVolumesMap
 from tests.e2e.utilities.results.results import (
     BuilderContainers,
     LocalMounts,
     OT3EmulatorContainers,
+    OT3EmulatorNamedVolumes,
     OT3Results,
     Result,
+    SystemBuildArgs,
 )
 from tests.e2e.utilities.system_test_definition import SystemTestDefinition
 
@@ -37,36 +40,64 @@ class E2EEvaluator:
             ot3_firmware_mounted=self._test_def.local_ot3_firmware_mounted,
             opentrons_modules_mounted=self._test_def.local_opentrons_modules_mounted
         )
+
+    def _evaluate_expected_system_build_args(self) -> SystemBuildArgs:
+        return SystemBuildArgs(
+            monorepo_build_args=self._test_def.monorepo_build_args,
+            ot3_firmware_build_args=self._test_def.ot3_firmware_build_args,
+            opentrons_modules_build_args=self._test_def.opentrons_modules_build_args,
+        )
+
+    @staticmethod
+    def _evaluate_expected_ot3_emulator_named_volumes() -> OT3EmulatorNamedVolumes:
+        return OT3EmulatorNamedVolumes(
+            head_volume_exists=True,
+            gantry_x_volume_exists=True,
+            gantry_y_volume_exists=True,
+            gripper_volume_exists=True,
+            pipettes_volume_exists=True,
+            bootloader_volume_exists=True
+        )
     @property
     def ot3_validation_required(self) -> bool:
         return self._test_def.ot3_firmware_builder_created
 
     def generate_expected_result(self) -> Result:
         expected_ot3_containers = self._evaluate_expected_ot3_containers()
-        expected_ot3_results = OT3Results(containers=expected_ot3_containers)
+        expected_ot3_emulator_volumes = self._evaluate_expected_ot3_emulator_named_volumes()
+        expected_ot3_results = OT3Results(
+            containers=expected_ot3_containers,
+            emulator_volumes=expected_ot3_emulator_volumes
+        )
         builder_containers = self._evaluate_expected_builder_containers()
         local_mounts = self._evaluate_expected_local_mounts()
+        expected_build_args = self._evaluate_expected_system_build_args()
 
         return Result(
             ot3_results=expected_ot3_results,
             builder_containers=builder_containers,
-            local_mounts=local_mounts
+            local_mounts=local_mounts,
+            system_build_args=expected_build_args
         )
 
     def generate_actual_result(self) -> Result:
         ...
         if self.ot3_validation_required:
             actual_ot3_containers = OT3EmulatorContainers.from_system_under_test(self._system_under_test)
+            actual_ot3_emulator_volumes = OT3EmulatorNamedVolumes.from_system_under_test(self._system_under_test)
         else:
             actual_ot3_containers = None
+            actual_ot3_emulator_volumes = None
 
         actual_ot3_results = OT3Results(
-            containers=actual_ot3_containers
+            containers=actual_ot3_containers,
+            emulator_volumes=actual_ot3_emulator_volumes
         )
         return Result(
             ot3_results=actual_ot3_results,
             builder_containers=BuilderContainers.from_system_under_test(self._system_under_test),
-            local_mounts=LocalMounts.from_system_under_test(self._system_under_test)
+            local_mounts=LocalMounts.from_system_under_test(self._system_under_test),
+            system_build_args=SystemBuildArgs.from_system_under_test(self._system_under_test)
         )
 
 
--- emulation_system/tests/e2e/utilities/results/results.py
+++ emulation_system/tests/e2e/utilities/results/results.py
@@ -2,7 +2,13 @@ from dataclasses import dataclass, fields
 from typing import Optional
 
 from tests.e2e.fixtures.ot3_containers import OT3SystemUnderTest
-from tests.e2e.utilities.consts import ExpectedNamedVolume, ExpectedMount
+from tests.e2e.utilities.build_arg_configurations import BuildArgConfigurations
+from tests.e2e.utilities.consts import (
+    ExpectedNamedVolume,
+    ExpectedMount,
+    OT3FirmwareEmulatorNamedVolumesMap,
+)
+from tests.e2e.utilities.helper_functions import confirm_named_volume_exists
 
 
 @dataclass
@@ -69,17 +75,55 @@ class OT3Mounts:
 
 
 @dataclass
-class OT3NamedVolumes:
+class OT3EmulatorNamedVolumes:
 
-    head: ExpectedNamedVolume
-    gantry_x: ExpectedNamedVolume
-    gantry_y: ExpectedNamedVolume
-    gripper: ExpectedNamedVolume
-    pipettes: ExpectedNamedVolume
-    bootloader: ExpectedNamedVolume
+    head_volume_exists: bool
+    gantry_x_volume_exists: bool
+    gantry_y_volume_exists: bool
+    gripper_volume_exists: bool
+    pipettes_volume_exists: bool
+    bootloader_volume_exists: bool
+
+    @classmethod
+    def from_system_under_test(
+        cls, system_under_test: OT3SystemUnderTest
+    ) -> "OT3EmulatorNamedVolumes":
+        return cls(
+            head_volume_exists=confirm_named_volume_exists(
+                system_under_test.head,
+                OT3FirmwareEmulatorNamedVolumesMap.HEAD
+            ),
+            gantry_x_volume_exists=confirm_named_volume_exists(
+                system_under_test.gantry_x,
+                OT3FirmwareEmulatorNamedVolumesMap.GANTRY_X
+            ),
+            gantry_y_volume_exists=confirm_named_volume_exists(
+                system_under_test.gantry_y,
+                OT3FirmwareEmulatorNamedVolumesMap.GANTRY_Y
+            ),
+            gripper_volume_exists=confirm_named_volume_exists(
+                system_under_test.gripper,
+                OT3FirmwareEmulatorNamedVolumesMap.GRIPPER
+            ),
+            pipettes_volume_exists=confirm_named_volume_exists(
+                system_under_test.pipettes,
+                OT3FirmwareEmulatorNamedVolumesMap.PIPETTES
+            ),
+            bootloader_volume_exists=confirm_named_volume_exists(
+                system_under_test.bootloader,
+                OT3FirmwareEmulatorNamedVolumesMap.BOOTLOADER
+            )
+        )
+
+
+@dataclass
+class OT3StateManagerNamedVolumes:
 
     state_manager_monorepo: ExpectedNamedVolume
     state_manager_virtual_env: ExpectedNamedVolume
+
+@dataclass
+class OT3FirmwareBuilderNamedVolumes:
 
     ot3_firmware_builder_head: ExpectedNamedVolume
     ot3_firmware_builder_gantry_x: ExpectedNamedVolume
@@ -100,6 +144,7 @@ class OT3ContainersWithMonorepoWheelVolume:
 @dataclass
 class OT3Results:
     containers: OT3EmulatorContainers
+    emulator_volumes: OT3EmulatorNamedVolumes
     # binaries: OT3Binaries
     # containers_with_monorepo_volumes: OT3ContainersWithMonorepoWheelVolume
 
@@ -137,6 +182,22 @@ class LocalMounts:
             opentrons_modules_mounted=system_under_test.local_opentrons_modules_mounted,
         )
 
+@dataclass
+class SystemBuildArgs:
+    monorepo_build_args: BuildArgConfigurations
+    ot3_firmware_build_args: BuildArgConfigurations
+    opentrons_modules_build_args: BuildArgConfigurations
+
+    @classmethod
+    def from_system_under_test(
+        cls,
+        system_under_test: OT3SystemUnderTest
+    ) -> "SystemBuildArgs":
+        return cls(
+            monorepo_build_args=system_under_test.monorepo_build_args,
+            ot3_firmware_build_args=system_under_test.ot3_firmware_build_args,
+            opentrons_modules_build_args=system_under_test.opentrons_modules_build_args
+        )
 
 @dataclass
 class Result:
@@ -144,3 +205,4 @@ class Result:
     ot3_results: Optional[OT3Results]
     builder_containers: BuilderContainers
     local_mounts: LocalMounts
+    system_build_args: SystemBuildArgs
