5ec654c17cfa098e5a991038b7298c53684b97f8
Derek Maggio <derek.maggio@opentrons.com>
2023-05-09 11:53:42 -0700

    Rework build scripts
    All build scripts: 
    - Add more progress updates during build steps 
    - Standardize any generated directory names to be kebab case and not snake case
    - Change any rm commands to remove everything in volume directory instead of hardcoded simulator file
    
    monorepo_builder.sh:
    - Make building bdist_wheels quieter
    
    opentrons_modules_builder.sh:
    - Break up building opentrons modules and creating simulators to allow for more progress updates
    
    ot3_firmware_builder.sh:
    - Breakup building ot3-firmware, creating simulators, and building state manager to allow for more progress updates
    - Add building of state manager wheel

--- docker/entrypoints/ot3_firmware_builder.sh
+++ docker/entrypoints/ot3_firmware_builder.sh
@@ -6,31 +6,61 @@
 # On each OT-3 Firmware emulator container attach its respective volume
 
 echo "Building ot3-firmware"
-
 (
   cd /ot3-firmware && \
-  cmake --preset host-gcc10 && \
+  cmake --preset host-gcc10
+)
+
+echo "Building subsystem simulator files"
+(
+  cd /ot3-firmware && \
   cmake --build ./build-host -j $(expr $(nproc) - 1)
 )
 
-echo "Creating simulator directories"
+echo "Building ot3-firmware State Manager"
+(
+  cd /ot3-firmware && \
+  cmake --build --preset tests --target state-manager-build
+)
 
+
+echo "Creating simulator directories (If needed)"
 mkdir -p \
-  /volumes/pipettes_volume \
-  /volumes/head_volume \
-  /volumes/gantry_x_volume \
-  /volumes/gantry_y_volume \
-  /volumes/gantry_y_volume \
-  /volumes/bootloader_volume \
-  /volumes/gripper_volume \
-  /volumes/state_manager_venv
+  /volumes/pipettes-volume \
+  /volumes/head-volume \
+  /volumes/gantry-x-volume \
+  /volumes/gantry-y-volume \
+  /volumes/gantry-y-volume \
+  /volumes/bootloader-volume \
+  /volumes/gripper-volume \
+  /volumes/state-manager-venv \
+  /volumes/state-manager-dist
 
-echo "Copying simulator files to simulator directories"
+echo "Removing any existing simulators from simulator directories"
+rm -f /volumes/pipettes-volume/*
+rm -f /volumes/head-volume/*
+rm -f /volumes/gantry-x-volume/*
+rm -f /volumes/gantry-y-volume/*
+rm -f /volumes/bootloader-volume/*
+rm -f /volumes/gripper-volume/*
+rm -rf /volumes/state-manager-venv/*
+rm -rf /volumes/state-manager-dist/*
 
-cp /ot3-firmware/build-host/pipettes/simulator/pipettes-single-simulator /volumes/pipettes_volume/pipettes-simulator
-cp /ot3-firmware/build-host/head/simulator/head-simulator /volumes/head_volume/head-simulator
-cp /ot3-firmware/build-host/gantry/simulator/gantry-x-simulator /volumes/gantry_x_volume/gantry-x-simulator
-cp /ot3-firmware/build-host/gantry/simulator/gantry-y-simulator /volumes/gantry_y_volume/gantry-y-simulator
-cp /ot3-firmware/build-host/bootloader/simulator/bootloader-simulator /volumes/bootloader_volume/bootloader-simulator
-cp /ot3-firmware/build-host/gripper/simulator/gripper-simulator /volumes/gripper_volume/gripper-simulator
-cp -r /ot3-firmware/build-host/.venv /volumes/state_manager_venv/
+echo "Copying built simulator files to simulator directories"
+cp /ot3-firmware/build-host/pipettes/simulator/pipettes-single-simulator /volumes/pipettes-volume/pipettes-simulator
+cp /ot3-firmware/build-host/head/simulator/head-simulator /volumes/head-volume/head-simulator
+cp /ot3-firmware/build-host/gantry/simulator/gantry-x-simulator /volumes/gantry-x-volume/gantry-x-simulator
+cp /ot3-firmware/build-host/gantry/simulator/gantry-y-simulator /volumes/gantry-y-volume/gantry-y-simulator
+cp /ot3-firmware/build-host/bootloader/simulator/bootloader-simulator /volumes/bootloader-volume/bootloader-simulator
+cp /ot3-firmware/build-host/gripper/simulator/gripper-simulator /volumes/gripper-volume/gripper-simulator
+cp -r /ot3-firmware/build-host/.venv/* /volumes/state-manager-venv/
+cp -r /ot3-firmware/state_manager/dist/* /volumes/state-manager-dist/
+
+cp /ot3-firmware/build-host/pipettes/simulator/pipettes-single-simulator /volumes/pipettes-volume/pipettes-simulator
+cp /ot3-firmware/build-host/head/simulator/head-simulator /volumes/head-volume/head-simulator
+cp /ot3-firmware/build-host/gantry/simulator/gantry-x-simulator /volumes/gantry-x-volume/gantry-x-simulator
+cp /ot3-firmware/build-host/gantry/simulator/gantry-y-simulator /volumes/gantry-y-volume/gantry-y-simulator
+cp /ot3-firmware/build-host/bootloader/simulator/bootloader-simulator /volumes/bootloader-volume/bootloader-simulator
+cp /ot3-firmware/build-host/gripper/simulator/gripper-simulator /volumes/gripper-volume/gripper-simulator
+cp -r /ot3-firmware/build-host/.venv/* /volumes/state-manager-venv/
+cp -r /ot3-firmware/state_manager/dist/* /volumes/state-manager-dist/
