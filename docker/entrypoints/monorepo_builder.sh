411de75fc13416b615bf508798e52a78c71bab40
Derek Maggio <derek.maggio@opentrons.com>
2023-05-09 11:01:20 -0700

    Monorepo builder.... sssshhhh

--- docker/entrypoints/monorepo_builder.sh
+++ docker/entrypoints/monorepo_builder.sh
@@ -8,10 +8,9 @@
 
 # /dist will be loaded into a volume in local-monorepo-builder container
 # All emulator containers needing the wheels will use that volume and bind it to /dist
-
-(cd /opentrons/shared-data/python && monorepo_python setup.py bdist_wheel -d /dist/)
-(cd /opentrons/api && monorepo_python setup.py bdist_wheel -d /dist/)
-(cd /opentrons/notify-server && monorepo_python setup.py bdist_wheel -d /dist/)
-(cd /opentrons/robot-server && monorepo_python setup.py bdist_wheel -d /dist/)
-(cd /opentrons/hardware && monorepo_python setup.py bdist_wheel -d /dist/)
-(cd /opentrons/server-utils && monorepo_python setup.py bdist_wheel -d /dist/)
+(cd /opentrons/shared-data/python && monorepo_python setup.py -q bdist_wheel -d /dist/)
+(cd /opentrons/api && monorepo_python setup.py -q bdist_wheel -d /dist/)
+(cd /opentrons/notify-server && monorepo_python setup.py -q bdist_wheel -d /dist/)
+(cd /opentrons/robot-server && monorepo_python setup.py -q bdist_wheel -d /dist/)
+(cd /opentrons/hardware && monorepo_python setup.py -q bdist_wheel -d /dist/)
+(cd /opentrons/server-utils && monorepo_python setup.py -q bdist_wheel -d /dist/)
