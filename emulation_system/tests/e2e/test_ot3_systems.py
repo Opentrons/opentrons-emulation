ebbfbeb0338902da34ee311c445e71f9958df0a5
Derek Maggio <derek.maggio@opentrons.com>
2023-05-10 10:47:34 -0700

    Rework test logic
    - Instead of writing all the custom comparison logic just compare 2 dictionaries
    - Use E2EHostSystem as fixture instead of all OT3Cntainers, ModuleContainers, ExpectedBindMounts etc...
    - Add custom dictionary parsing function because dataclasses.asdict doesn't like sets

--- emulation_system/tests/e2e/test_ot3_systems.py
+++ emulation_system/tests/e2e/test_ot3_systems.py
@@ -1,29 +1,66 @@
 """Location for OT-3 e2e pytest functions."""
-from typing import Callable
+import copy
+from dataclasses import fields, is_dataclass
+from typing import Any, Callable, Dict
 
 import pytest
 
-from tests.e2e.fixtures.expected_bind_mounts import ExpectedBindMounts
-from tests.e2e.fixtures.module_containers import ModuleContainers
-from tests.e2e.fixtures.ot3_containers import OT3Containers
-from tests.e2e.test_mappings import get_e2e_test_parmeters
-from tests.e2e.utilities.system_test_definition import SystemTestDefinition
+from tests.e2e.docker_interface.e2e_system import E2EHostSystem
+from tests.e2e.test_definition.system_test_definition import SystemTestDefinition
+from tests.e2e.test_mappings import get_e2e_test_parameters
+from e2e.results import FinalResult
 
 
-@pytest.mark.parametrize("test_def", get_e2e_test_parmeters())
+def __inner_convert_to_dict(obj):  # noqa: ANN001, ANN203
+    """Method to handle converting dataclass into raw values.
+
+    Base logic pulled from source code of dataclasses.asdict.
+    Added handling for set objects. Removed handling for named tuples
+    """
+    if is_dataclass(obj):
+        result = []
+        for field in fields(obj):
+            value = __inner_convert_to_dict(getattr(obj, field.name))
+            result.append((field.name, value))
+        return dict(result)
+    elif isinstance(obj, (list, tuple)):
+        return type(obj)(__inner_convert_to_dict(v) for v in obj)
+    elif isinstance(obj, set):
+        return {
+            frozenset((k, v) for k, v in __inner_convert_to_dict(instance).items())
+            for instance in obj
+        }
+
+    elif isinstance(obj, dict):
+        return dict(
+            (__inner_convert_to_dict(k), __inner_convert_to_dict(v))
+            for k, v in obj.items()
+        )
+    else:
+        return copy.deepcopy(obj)
+
+
+# dataclass doesn't have a built-in type because it is a transformation
+def convert_to_dict(obj) -> Dict[str, Any]:  # noqa: ANN001
+    """Converts dataclass objects into raw values of dicts, list, sets, and tuples."""
+    if not is_dataclass(obj):
+        raise TypeError("Must pass dataclass type")
+    return __inner_convert_to_dict(obj)
+
+
+@pytest.mark.parametrize("test_def", get_e2e_test_parameters())
 def test_e2e(
-    test_def: SystemTestDefinition,
-    ot3_model_under_test: Callable[[str], OT3Containers],
-    modules_under_test: Callable[[str], ModuleContainers],
-    local_mounts_under_test: Callable[[str], ExpectedBindMounts],
+    test_def: SystemTestDefinition, e2e_host: Callable[[str], E2EHostSystem]
 ) -> None:
     """Runs e2e tests for OT-3.
 
     If there is a failure, will log custom test output to console.
     """
-    ot3_system = ot3_model_under_test(test_def.yaml_config_relative_path)
-    modules = modules_under_test(test_def.yaml_config_relative_path)
-    mounts = local_mounts_under_test(test_def.yaml_config_relative_path)
-    test_def.compare(ot3_system, modules, mounts)
-    assert not test_def.is_failure(), test_def.print_output()
-    print(test_def.print_output())
+    e2e_system = e2e_host(test_def.yaml_config_relative_path)
+    actual_results = convert_to_dict(
+        FinalResult.get_actual_results(e2e_system).ot3_results
+    )
+    expected_results = convert_to_dict(
+        FinalResult.get_expected_results(test_def).ot3_results
+    )
+    assert actual_results == expected_results
