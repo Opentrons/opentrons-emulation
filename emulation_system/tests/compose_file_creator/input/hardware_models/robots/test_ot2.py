bfaa97a85bc1ccb7769238f0e43a2c2e04016116
Derek Maggio <derek.maggio@opentrons.com>
2023-06-08 09:37:03 -0700

    test: Modify OT-2 tests to account for change in syntax of specifying pipettes

--- emulation_system/tests/compose_file_creator/input/hardware_models/robots/test_ot2.py
+++ emulation_system/tests/compose_file_creator/input/hardware_models/robots/test_ot2.py
@@ -31,13 +31,8 @@ def ot2_bad_emulation_level(ot2_model: Dict[str, Any]) -> Dict[str, Any]:
 def ot2_with_pipettes(ot2_model: Dict[str, Any]) -> Dict[str, Any]:
     """OT-2 using user-specified pipettes."""
     hardware_specific_attributes = ot2_model["hardware-specific-attributes"]
-    hardware_specific_attributes["left-pipette"] = {}
-    hardware_specific_attributes["left-pipette"]["model"] = "test_1"
-    hardware_specific_attributes["left-pipette"]["id"] = "test_1_id"
-
-    hardware_specific_attributes["right-pipette"] = {}
-    hardware_specific_attributes["right-pipette"]["model"] = "test_2"
-    hardware_specific_attributes["right-pipette"]["id"] = "test_2_id"
+    hardware_specific_attributes["left-pipette"] = "P20 Single"
+    hardware_specific_attributes["right-pipette"] = "P300 Multi"
     return ot2_model
 
 
@@ -49,10 +44,8 @@ def test_default_ot2(ot2_model: Dict[str, Any]) -> None:
     assert ot2.emulation_level == EmulationLevels.FIRMWARE.value
     assert ot2.exposed_port == 5000
     assert ot2.bound_port == 31950
-    assert ot2.hardware_specific_attributes.left_pipette.model == "p20_single_v2.0"
-    assert ot2.hardware_specific_attributes.left_pipette.id == "P20SV202020070101"
-    assert ot2.hardware_specific_attributes.right_pipette.model == "p20_single_v2.0"
-    assert ot2.hardware_specific_attributes.right_pipette.id == "P20SV202020070101"
+    assert ot2.hardware_specific_attributes.left_pipette is None
+    assert ot2.hardware_specific_attributes.right_pipette is None
     assert ot2.source_repos.firmware_repo_name == OpentronsRepository.OPENTRONS
     assert ot2.source_repos.hardware_repo_name is None
 
@@ -67,10 +60,8 @@ def test_ot2_with_overridden_bound_port(
     assert ot2.emulation_level == EmulationLevels.FIRMWARE.value
     assert ot2.exposed_port == 5000
     assert ot2.bound_port == 2500
-    assert ot2.hardware_specific_attributes.left_pipette.model == "p20_single_v2.0"
-    assert ot2.hardware_specific_attributes.left_pipette.id == "P20SV202020070101"
-    assert ot2.hardware_specific_attributes.right_pipette.model == "p20_single_v2.0"
-    assert ot2.hardware_specific_attributes.right_pipette.id == "P20SV202020070101"
+    assert ot2.hardware_specific_attributes.left_pipette is None
+    assert ot2.hardware_specific_attributes.right_pipette is None
 
 
 def test_ot2_with_custom_pipettes(ot2_with_pipettes: Dict[str, Any]) -> None:
@@ -81,10 +72,8 @@ def test_ot2_with_custom_pipettes(ot2_with_pipettes: Dict[str, Any]) -> None:
     assert ot2.emulation_level == EmulationLevels.FIRMWARE.value
     assert ot2.exposed_port == 5000
     assert ot2.bound_port == 31950
-    assert ot2.hardware_specific_attributes.left_pipette.model == "test_1"
-    assert ot2.hardware_specific_attributes.left_pipette.id == "test_1_id"
-    assert ot2.hardware_specific_attributes.right_pipette.model == "test_2"
-    assert ot2.hardware_specific_attributes.right_pipette.id == "test_2_id"
+    assert ot2.hardware_specific_attributes.left_pipette == "P20 Single"
+    assert ot2.hardware_specific_attributes.right_pipette == "P300 Multi"
 
 
 def test_ot2_with_bad_emulation_level(ot2_bad_emulation_level: Dict[str, Any]) -> None:
