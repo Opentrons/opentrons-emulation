f50d1cc83238a2c202b40f94beecb334de9ae43a
Derek Maggio <derek.maggio@opentrons.com>
2023-06-27 15:09:19 +0000

    chore: Add new deps

--- scripts/docker_convenience_scripts/create_dev_dockerfile.sh
+++ scripts/docker_convenience_scripts/create_dev_dockerfile.sh
@@ -8,20 +8,23 @@ else
 fi
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-MODDED_BASES_DOCKERFILE=`sed \
+
+MODDED_BASES_DOCKERFILE=$(sed \
   -e "s|ubuntu-base|ubuntu-base-dev|g" \
   -e "s|python-base|python-base-dev|g" \
   -e "s|cpp-base|cpp-base-dev|g" \
-  ${SCRIPT_DIR}/../../docker/bases_Dockerfile`
+  ${SCRIPT_DIR}/../../docker/bases_Dockerfile)
 
-MODDED_DOCKERFILE=`sed \
-  -e 's|ghcr.io/opentrons/ubuntu-base-${TARGETARCH}:latest|ubuntu-base-dev-'${arch_to_use}'|g' \
+
+MODDED_DOCKERFILE=$(sed \
+  -e 's|ghcr.io/opentrons/ubuntu-base-${TARGETARCH}|ubuntu-base-dev-'${arch_to_use}'|g' \
   -e 's|ghcr.io/opentrons/cpp-base-${TARGETARCH}:latest|cpp-base-dev-'${arch_to_use}'|g' \
+  -e 's|ghcr.io/opentrons/cpp-base-${TARGETARCH}|cpp-base-dev-'${arch_to_use}'|g' \
   -e "s|ghcr.io/opentrons/python-base:latest|python-base-dev|g" \
   -e "s|ghcr.io/opentrons/cpp-base:latest|cpp-base-dev|g" \
-  ${SCRIPT_DIR}/../../docker/Dockerfile`
+  ${SCRIPT_DIR}/../../docker/Dockerfile)
 
-COMBINED_DOCKERFILE=`printf "${MODDED_BASES_DOCKERFILE}\n\n${MODDED_DOCKERFILE}"`
+COMBINED_DOCKERFILE=$(printf "${MODDED_BASES_DOCKERFILE}\n\n${MODDED_DOCKERFILE}")
 
 # Move syntax string to top of new file
 printf "${COMBINED_DOCKERFILE}" | sed \
