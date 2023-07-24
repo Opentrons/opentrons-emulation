c0e4eab5b5ddf54093b7c7513f42a68f63ba333c
Derek Maggio <derek.maggio@opentrons.com>
2023-07-24 13:18:42 -0700

    Add logic to check all container-names passed to extra-mounts are valid

--- emulation_system/tests/compose_file_creator/conversion_logic/test_extra_mounts.py
+++ emulation_system/tests/compose_file_creator/conversion_logic/test_extra_mounts.py
@@ -1,7 +1,8 @@
 """Tests related to extra-mounts on services"""
-
 from pathlib import Path
-from typing import Any, Dict
+from typing import Any, Dict, List
+
+import pytest
 
 from emulation_system.compose_file_creator.conversion.conversion_functions import (
     convert_from_obj,
@@ -9,16 +10,34 @@ from emulation_system.compose_file_creator.conversion.conversion_functions impor
 from tests.validation_helper_functions import mount_string_is
 
 
-def test_extra_mounts(tmp_path: Path, ot3_only: Dict[str, Any]) -> None:
-    """Test extra mounts added correctly"""
+@pytest.fixture
+def extra_file_mount(
+    tmp_path: Path,
+) -> Path:
+    """Create a file to mount."""
     extra_file_mount = tmp_path / "envs/extra.env"
     extra_file_mount.parent.mkdir()
     extra_file_mount.touch()
+    return extra_file_mount
 
+
+@pytest.fixture
+def extra_dir_mount(
+    tmp_path: Path,
+) -> Path:
+    """Create a directory to mount."""
     extra_dir_mount = tmp_path / "extra_dir"
     extra_dir_mount.mkdir()
+    return extra_dir_mount
 
-    ot3_only["extra-mounts"] = [
+
+@pytest.fixture
+def extra_mounts(
+    extra_file_mount: Path,
+    extra_dir_mount: Path,
+) -> List[Dict[str, Any]]:
+    """Create a list of extra mounts."""
+    return [
         {
             "container-names": ["edgar-allen-poebot", "ot3-state-manager"],
             "host-path": str(extra_file_mount),
@@ -31,6 +50,35 @@ def test_extra_mounts(tmp_path: Path, ot3_only: Dict[str, Any]) -> None:
         },
     ]
 
+
+@pytest.fixture
+def extra_mounts_with_bad_container_names(
+    extra_file_mount: Path,
+    extra_dir_mount: Path,
+) -> List[Dict[str, Any]]:
+    """Create a list of extra mounts with bad container names."""
+    return [
+        {
+            "container-names": ["no-bot", "ot3-state-imploder"],
+            "host-path": str(extra_file_mount),
+            "container-path": "/extra.env",
+        },
+        {
+            "container-names": ["ot3-foot", "emulator-pixie"],
+            "host-path": str(extra_dir_mount),
+            "container-path": "/extra_dir",
+        },
+    ]
+
+
+def test_extra_mounts(
+    extra_file_mount: Path,
+    extra_dir_mount: Path,
+    extra_mounts: List[Dict[str, Any]],
+    ot3_only: Dict[str, Any],
+) -> None:
+    """Test extra mounts added correctly"""
+    ot3_only["extra-mounts"] = extra_mounts
     converted_object = convert_from_obj(ot3_only, dev=False)
     robot_server = converted_object.robot_server
     state_manager = converted_object.ot3_state_manager
@@ -47,3 +95,18 @@ def test_extra_mounts(tmp_path: Path, ot3_only: Dict[str, Any]) -> None:
         assert container is not None
         assert not mount_string_is(f"{str(extra_file_mount)}:/extra.env", container)
         assert mount_string_is(f"{str(extra_dir_mount)}:/extra_dir", container)
+
+
+def test_exception_thrown_on_invalid_container_names(
+    extra_mounts_with_bad_container_names: List[Dict[str, Any]],
+    ot3_only: Dict[str, Any],
+) -> None:
+    """Test that exception is thrown if extra mounts have invalid container names."""
+    ot3_only["extra-mounts"] = extra_mounts_with_bad_container_names
+    with pytest.raises(ValueError) as error_info:
+        convert_from_obj(ot3_only, dev=False)
+
+    assert (
+        'The following "container_names" specified in "extra-mounts" do not exist in the emulated system:'
+        in str(error_info.value)
+    )
