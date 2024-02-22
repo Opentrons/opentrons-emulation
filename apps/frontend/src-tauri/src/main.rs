4310dfb1e2e1d249ab8d99a4121308e7b155286c
Derek Maggio <derek.j.maggio@gmail.com>
2024-02-21 16:10:36 -0800

    Run mosquitto

--- apps/frontend/src-tauri/src/main.rs
+++ apps/frontend/src-tauri/src/main.rs
@@ -1,6 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod mosquitto;
+
 fn main() {
+  let _ = mosquitto::mosquitto();
   app_lib::run();
 }
